727de2e4addfc9d8d7ab71d5bb37344a83e0331d
the-Astra <djs8743@rit.edu>
2026-04-01 14:20:33 -0400

    Setup credits

--- content/absinthe/init_absinthe.lua
+++ content/absinthe/init_absinthe.lua
@@ -1,3 +1,10 @@
+SMODS.Atlas {
+    key = "abs_credits",
+    path = 'absinthe/abs_credits.png',
+    px = 71,
+    py = 95
+}
+
 PotatoPatchUtils.Team {
     name = 'absinthe',
     colour = HEX('5fd47e')
@@ -6,36 +13,57 @@ PotatoPatchUtils.Team {
 PotatoPatchUtils.Developer {
     name = 'pangaea47',
     team = 'absinthe',
+    atlas = 'worm_abs_credits',
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 1, y = 0 }
 }
 
 PotatoPatchUtils.Developer {
     name = 'theAstra',
     team = 'absinthe',
-    colour = HEX('8710c7')
+    colour = HEX('8710c7'),
+    atlas = 'worm_abs_credits',
+    pos = { x = 2, y = 0 },
+    soul_pos = { x = 3, y = 0 }
 }
 
 PotatoPatchUtils.Developer {
     name = 'iwas_nevergood',
     team = 'absinthe',
+    atlas = 'worm_abs_credits',
+    pos = { x = 2, y = 1 },
+    soul_pos = { x = 3, y = 1 }
 }
 
 PotatoPatchUtils.Developer {
     name = 'pi_cubed',
     team = 'absinthe',
+    atlas = 'worm_abs_credits',
+    pos = { x = 0, y = 2 },
+    soul_pos = { x = 1, y = 2 }
 }
 
 PotatoPatchUtils.Developer {
     name = 'AnneBean',
     team = 'absinthe',
+    atlas = 'worm_abs_credits',
+    pos = { x = 2, y = 2 },
+    soul_pos = { x = 3, y = 2 }
 }
 
 PotatoPatchUtils.Developer {
     name = 'nixthatoneartist',
     team = 'absinthe',
     colour = HEX("0db829"),
+    atlas = 'worm_abs_credits',
+    pos = { x = 0, y = 3 },
+    soul_pos = { x = 1, y = 3 }
 }
 
 PotatoPatchUtils.Developer {
     name = 'base4',
     team = 'absinthe',
+    atlas = 'worm_abs_credits',
+    pos = { x = 0, y = 1 },
+    soul_pos = { x = 1, y = 1 }
 }
