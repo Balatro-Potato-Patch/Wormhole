34ce9ef708ff061681c37635b56255479f82ee18
the-Astra <djs8743@rit.edu>
2026-04-01 12:31:49 -0400

    Dev and Team Objects

---
+++ content/absinthe/init_absinthe.lua
@@ -0,0 +1,40 @@
+PotatoPatchUtils.Team {
+    name = 'absinthe',
+    colour = HEX('5fd47e')
+}
+
+PotatoPatchUtils.Developer {
+    name = 'pangaea47',
+    team = 'absinthe',
+}
+
+PotatoPatchUtils.Developer {
+    name = 'theAstra',
+    team = 'absinthe',
+    colour = HEX('8710c7')
+}
+
+PotatoPatchUtils.Developer {
+    name = 'iwas_nevergood',
+    team = 'absinthe',
+}
+
+PotatoPatchUtils.Developer {
+    name = 'pi_cubed',
+    team = 'absinthe',
+}
+
+PotatoPatchUtils.Developer {
+    name = 'AnneBean',
+    team = 'absinthe',
+}
+
+PotatoPatchUtils.Developer {
+    name = 'NixThatOneArtist',
+    team = 'absinthe',
+}
+
+PotatoPatchUtils.Developer {
+    name = 'base4',
+    team = 'absinthe',
+}
