2d88faa3588d9f937ca914d0dcd476d374223e69
Eremel <richardbailey@outlook.com>
2026-04-01 23:22:31 +0100

    Initial team/dev set up

---
+++ content/taxes_back_pain/main.lua
@@ -0,0 +1,47 @@
+PotatoPatchUtils.Team({
+    name = 'tbp',
+    loc = true,
+    colour = HEX('99acad'),
+})
+
+PotatoPatchUtils.Developer({
+    name = 'eremel',
+    team = 'tbp',
+    loc = true,
+})
+
+PotatoPatchUtils.Developer({
+    name = 'n',
+    team = 'tbp',
+    loc = true,
+})
+
+PotatoPatchUtils.Developer({
+    name = 'sdm',
+    team = 'tbp',
+    loc = true,
+})
+
+PotatoPatchUtils.Developer({
+    name = 'dilly',
+    team = 'tbp',
+    loc = true,
+})
+
+PotatoPatchUtils.Developer({
+    name = 'ice',
+    team = 'tbp',
+    loc = true,
+})
+
+PotatoPatchUtils.Developer({
+    name = 'rsnow',
+    team = 'tbp',
+    loc = true,
+})
+
+PotatoPatchUtils.Developer({
+    name = 'mythie',
+    team = 'tbp',
+    loc = true,
+})
---
+++ localization/taxes_back_pain/en-us.lua
@@ -0,0 +1,49 @@
+return {
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_tbp = {name = 'Taxes and Back Pain'},
+            PotatoPatchDev_eremel = {
+                name = 'Eremel',
+                text = {
+                    'placeholder'
+                }
+            },
+            PotatoPatchDev_n = {
+                name = 'N\'',
+                text = {
+                    'placeholder'
+                }
+            },
+            PotatoPatchDev_sdm = {
+                name = 'SDM0',
+                text = {
+                    'placeholder'
+                }
+            },
+            PotatoPatchDev_dilly = {
+                name = 'DillyTheDillster',
+                text = {
+                    'placeholder'
+                }
+            },
+            PotatoPatchDev_ice = {
+                name = 'Ice',
+                text = {
+                    'placeholder'
+                }
+            },
+            PotatoPatchDev_rsnow = {
+                name = 'RattlingSnow',
+                text = {
+                    'placeholder'
+                }
+            },
+            PotatoPatchDev_mythie = {
+                name = 'itsmythie',
+                text = {
+                    'placeholder'
+                }
+            },
+        },
+    }
+}
