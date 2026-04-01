0bc09da2737e1fc85e4109e5b2ac77ddf085c8d3
RandomsongV2 <145283437+RandomsongV2@users.noreply.github.com>
2026-04-01 19:50:13 +0000

    team's menu added

---
+++ content/Team Shrug/main.lua
@@ -0,0 +1,32 @@
+-- [["¯\_(ツ)_/¯"]]
+PotatoPatchUtils.Team({
+    name = [[¯\_(ツ)_/¯]], -- todo add font for the smile
+    colour = HEX("B6334C"),
+    --colour = HEX("A65A60"), --low contrast variant
+    loc = true
+})
+PotatoPatchUtils.Developer({
+    team = [[¯\_(ツ)_/¯]],
+    name = "RandomsongV2",
+    colour = HEX("7b1414"),
+})
+PotatoPatchUtils.Developer({
+    team = [[¯\_(ツ)_/¯]],
+    name = "Microwave",
+    colour = HEX("845aad"),
+})
+PotatoPatchUtils.Developer({
+    team = [[¯\_(ツ)_/¯]],
+    name = "waffle",
+    colour = HEX("45ad71"),
+})
+PotatoPatchUtils.Developer({
+    team = [[¯\_(ツ)_/¯]],
+    name = "A tired guy",
+    colour = HEX("8f8275"),
+})
+PotatoPatchUtils.Developer({
+    team = [[¯\_(ツ)_/¯]],
+    name = "Edward Robinson",
+    colour = HEX("ffffff"),
+})
