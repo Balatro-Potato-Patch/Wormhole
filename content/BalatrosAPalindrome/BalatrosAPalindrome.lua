72e76d0ba5072dc51f4a0c829d3454a4d030ee50
Nogardagem <understateddragon@gmail.com>
2026-04-01 14:56:53 -0400

    working team list

---
+++ content/BalatrosAPalindrome/BalatrosAPalindrome.lua
@@ -0,0 +1,7 @@
+sendDebugMessage("BalatrosAPalindrome loading!")
+
+PotatoPatchUtils.Team({name="BalatrosAPalindrome", colour=G.C.BLUE})
+PotatoPatchUtils.Developer({team="BalatrosAPalindrome",name="Nogardagem"})
+PotatoPatchUtils.Developer({team="BalatrosAPalindrome",name="NerdyBread42"})
+PotatoPatchUtils.Developer({team="BalatrosAPalindrome",name="IzzyWizz"})
+PotatoPatchUtils.Developer({team="BalatrosAPalindrome",name="Knightingale0"})
