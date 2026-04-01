b704cfda8bcf902901e0cf461f688e097cf48952
the-Astra <djs8743@rit.edu>
2026-04-01 14:26:02 -0400

    Drinks consumable type (+ test obj)

---
+++ content/absinthe/drinks/drinks_main.lua
@@ -0,0 +1,14 @@
+SMODS.ConsumableType {
+    key = 'abs_drinks',
+    default = 'c_worm_abs_stargarita',
+    primary_colour = HEX('e3dbc8'),
+    secondary_colour = HEX('f5c242'),
+    collection_rows = { 5, 4 },
+    shop_rate = 0.8
+}
+
+SMODS.Consumable {
+    set = 'abs_drinks',
+    name = 'testname',
+    key = 'test_drink'
+}
