aec96af05cde18812273473a50e8ca7e434125ed
makatymba2001 <makatymba2001@yandex.ru>
2026-04-02 01:46:17 +0300

    Jtem 2 setup

---
+++ content/Jtem 2/1_main.lua
@@ -0,0 +1,54 @@
+PotatoPatchUtils.Team({
+	name = "jtem2",
+	colour = HEX("ff4267"),
+	loc = true,
+	calculate = function(self, context) end,
+})
+PotatoPatchUtils.Developer({
+	name = "aikoyori",
+	colour = G.C.ORANGE,
+	team = "jtem2",
+	loc = true,
+	-- pos = { x = 1, y = 0 },
+	-- atlas = "atlas_key",
+})
+PotatoPatchUtils.Developer({
+	name = "sleepyg11",
+	colour = G.C.BLUE,
+	team = "jtem2",
+	loc = true,
+	-- pos = { x = 1, y = 0 },
+	-- atlas = "atlas_key",
+})
+PotatoPatchUtils.Developer({
+	name = "haya",
+	colour = G.C.ORANGE,
+	team = "jtem2",
+	loc = true,
+	-- pos = { x = 1, y = 0 },
+	-- atlas = "atlas_key",
+})
+PotatoPatchUtils.Developer({
+	name = "lexi",
+	colour = G.C.ORANGE,
+	team = "jtem2",
+	loc = true,
+	-- pos = { x = 1, y = 0 },
+	-- atlas = "atlas_key",
+})
+PotatoPatchUtils.Developer({
+	name = "ari",
+	colour = G.C.ORANGE,
+	team = "jtem2",
+	loc = true,
+	-- pos = { x = 1, y = 0 },
+	-- atlas = "atlas_key",
+})
+PotatoPatchUtils.Developer({
+	name = "missingnumber",
+	colour = G.C.ORANGE,
+	team = "jtem2",
+	loc = true,
+	-- pos = { x = 1, y = 0 },
+	-- atlas = "atlas_key",
+})
---
+++ localization/Jtem 2/en-us.lua
@@ -0,0 +1,43 @@
+return {
+	descriptions = {
+		PotatoPatch = {
+			PotatoPatchTeam_jtem2 = { name = "Jtem 2" },
+			PotatoPatchDev_aikoyori = {
+				name = "Aikoyori",
+				text = {
+					{ "box numero 1" },
+				},
+			},
+			PotatoPatchDev_sleepyg11 = {
+				name = "SleepyG11",
+				text = {
+					{ "box numero 1" },
+				},
+			},
+			PotatoPatchDev_haya = {
+				name = "Haya",
+				text = {
+					{ "box numero 1" },
+				},
+			},
+			PotatoPatchDev_lexi = {
+				name = "Lexi",
+				text = {
+					{ "box numero 1" },
+				},
+			},
+			PotatoPatchDev_ari = {
+				name = "Ari",
+				text = {
+					{ "box numero 1" },
+				},
+			},
+			PotatoPatchDev_missingnumber = {
+				name = "MissingNumber",
+				text = {
+					{ "box numero 1" },
+				},
+			},
+		},
+	},
+}
