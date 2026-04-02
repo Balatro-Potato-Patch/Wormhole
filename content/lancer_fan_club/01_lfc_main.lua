00c5da5863096867c5ad34073d8048cdb2c8af1a
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2026-04-02 10:36:59 -0400

    rename lfc_main to ensure it loads first, add reference to team object

--- content/lancer_fan_club/01_lfc_main.lua
+++ content/lancer_fan_club/01_lfc_main.lua
@@ -1,4 +1,4 @@
-PotatoPatchUtils.Team {
+Wormhole.LancerFanClub = PotatoPatchUtils.Team {
     name = "Lancer Fan Club",
     loc = "worm_PotatoPatchTeam_lancer_fan_club",
     colour = HEX("5585bd") -- this was colorpicked directly from lancer's sprite
