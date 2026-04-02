39b2775bae0727d0c4a4db99e4e0d96e180997bd
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2026-04-02 09:50:36 -0400

    add team object

---
+++ content/lancer_fan_club/lfc_main.lua
@@ -0,0 +1,5 @@
+PotatoPatchUtils.Team {
+    name = "Lancer Fan Club",
+    loc = "worm_PotatoPatchTeam_lancer_fan_club",
+    colour = HEX("5585bd") -- this was colorpicked directly from lancer's sprite
+}
---
+++ localization/lancer_fan_club/en_us.lua
@@ -0,0 +1,18 @@
+return {
+    misc = {
+        dictionary = {
+
+        },
+        v_dictionary = {
+
+        },
+    },
+    descriptions = {
+        Joker = {
+
+        },
+        PotatoPatch = {
+            worm_PotatoPatchTeam_lancer_fan_club = "Lancer Fan Club",
+        },
+    },
+}
