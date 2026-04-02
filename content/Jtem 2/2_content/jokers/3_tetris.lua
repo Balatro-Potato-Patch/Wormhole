fd302e54806cc3f734ff5c633baf657fc30f22df
hayaunderscore <stophayapls123@proton.me>
2026-04-02 14:40:02 +0800

    feat(tgm): more work

--- content/Jtem 2/2_content/jokers/3_tetris.lua
+++ content/Jtem 2/2_content/jokers/3_tetris.lua
@@ -134,18 +134,177 @@ for name, piece in pairs(JtemTGM.pieces) do
 	end
 end
 
+--#region Constants & Util Functions
+
 -- A board is a 2D array of values, a character (corresponding to the piece type it came from) means its occupied
 -- if it is 0 then it is not occupied
 local BOARD_W, BOARD_H = 10, 20 -- Additional 5 blocks out of view, all pieces start at the 20th cell
 local BLOCK_W, BLOCK_H = 2, 2
 -- Clearance
 local BOARD_HCLEARANCE = -5
+-- Max level
+local MAX_LEVEL = 999
+
+-- All current states
+local STATE_NONE = 0
+local STATE_DROPPING = 1 -- Piece is dropping
+local STATE_LOCKING = 2  -- Piece has hit the ground, lock after a certain time, can go back to STATE_DROPPING
+local STATE_LOCKED = 3   -- Locked the piece in
+local STATE_ARE = 4      -- ARE checking
+local STATE_READY = 5    -- Getting ready
+local STATE_GO = 6
+local STATE_GAMEOVER = 7 -- Game is over
+local STATE_DROPOUT = 8  -- Puyo style drop out
+local STATE_CONGRATULATIONS = 9
+local STATE_YOUR = 10
+local STATE_AER = 11
+local STATE_GRADESHOW = 12
+local STATE_WAIT = 13 -- Waiting to try again
+local STATE_INVISIBLETETRIS = 14
 
 local function GtoSpeed(g)
 	return g / 256
 end
 
-local function ResetPlayerState()
+--#endregion
+
+--#region Piece randomizer
+
+-- Stripped version of the 35 bag randomizer from Cambridge
+-- https://github.com/cambridge-stacker/cambridge/blob/main/tetris/randomizers/history_6rolls_35bag.lua
+
+function JtemTGM.CreateRandomizer()
+	local self = {}
+	self.first = true
+	self.history = { "Z", "S", "Z", "S" }
+	self.pool = {
+		"I", "I", "I", "I", "I",
+		"T", "T", "T", "T", "T",
+		"L", "L", "L", "L", "L",
+		"J", "J", "J", "J", "J",
+		"S", "S", "S", "S", "S",
+		"Z", "Z", "Z", "Z", "Z",
+		"O", "O", "O", "O", "O",
+	}
+	self.droughts = {
+		I = 4,
+		T = 4,
+		L = 4,
+		J = 4,
+		S = 4,
+		Z = 4,
+		O = 4,
+	}
+	self.piece_index = {
+		"I",
+		"T",
+		"L",
+		"J",
+		"S",
+		"Z",
+		"O",
+	}
+	return self
+end
+
+local function updateHistory(self, shape)
+	table.remove(self.history, 1)
+	table.insert(self.history, shape)
+end
+
+local function checkHighDroughtCount(self)
+	local high_drought
+	local high_drought_count = 0
+	for k, v in pairs(self.piece_index) do
+		if self.droughts[v] >= high_drought_count then
+			high_drought = v
+			high_drought_count = self.droughts[v]
+		end
+	end
+	return high_drought_count
+end
+
+local function getMostDroughtedPiece(self)
+	local high_drought
+	local high_drought_count = 0
+	for k, v in pairs(self.piece_index) do
+		if self.droughts[v] >= high_drought_count then
+			high_drought = v
+			high_drought_count = self.droughts[v]
+		end
+	end
+	return high_drought
+end
+
+local function updateHistogram(self, shape)
+	for k, v in pairs(self.piece_index) do
+		if v == shape then
+			self.droughts[v] = 0
+		else
+			self.droughts[v] = self.droughts[v] + 1
+		end
+	end
+end
+
+local function inHistory(self, piece)
+	for idx, entry in pairs(self.history) do
+		if entry == piece then
+			return true
+		end
+	end
+	return false
+end
+
+function JtemTGM.GeneratePiece(self)
+	local index, x, highscore, did_reroll, did_first
+	did_reroll = false
+	did_first = false
+	if self.first then
+		index = pseudorandom("tetris", 1, 20)
+		x = self.pool[index]
+		self.first = false
+		did_first = true
+	else
+		for i = 1, 6 do
+			index = pseudorandom("tetris", 1, #self.pool)
+			x = self.pool[index]
+			if not inHistory(self, x) then
+				break
+			end
+			did_reroll = true -- checked laterer
+			self.pool[index] = getMostDroughtedPiece(self)
+			index = pseudorandom("tetris", 1, #self.pool)
+			x = self.pool[index]
+		end
+	end
+
+	highscore = checkHighDroughtCount(self)
+	updateHistory(self, x)
+	updateHistogram(self, x)
+
+	-- replicate TI bug
+	-- don't update for first piece, skip the other two tests. this is not the bug, as the first piece was not drawn from the bag.
+	if did_first then return x end
+
+	-- we should always update the bag here, but we only update it in two cases.
+	if highscore < checkHighDroughtCount(self) then
+		-- do update if the high drought count went up
+		self.pool[index] = getMostDroughtedPiece(self)
+	end
+	if not did_reroll then
+		-- do update if there is no reroll
+		self.pool[index] = getMostDroughtedPiece(self)
+	end
+
+	-- if neither happened, the bag does NOT get updated now. to remove the bug, comment ouut both ifs and one of the updates above, so the bag always updates except for first piece
+	return x
+end
+
+--#endregion
+
+--#region Actual stuff
+
+function JtemTGM.ResetPlayerState()
 	local board = {}
 	for y = BOARD_HCLEARANCE, BOARD_H - 1 do
 		board[y] = {}
@@ -153,8 +312,16 @@ local function ResetPlayerState()
 			board[y][x] = 0
 		end
 	end
-	return {
-		state = 0,          -- Literal current state
+	local randomizer = JtemTGM.CreateRandomizer()
+	local starting_piece = pseudorandom_element(JtemTGM.starting_pieces, "tetris_start")
+	local next_pieces = {
+		JtemTGM.GeneratePiece(randomizer),
+		JtemTGM.GeneratePiece(randomizer),
+		JtemTGM.GeneratePiece(randomizer),
+		JtemTGM.GeneratePiece(randomizer),
+	}
+	local state = {
+		state = STATE_NONE, -- Literal current state
 		state_complete = false, -- Checks if the state is considered as complete or not
 		state_timer = 0,    -- Current state timer
 
@@ -181,13 +348,14 @@ local function ResetPlayerState()
 		das_time = 0,       -- Current DAS time
 		das_delay = 12,     -- Current DAS
 
-		clears = 0,         -- Line clears
+		clears = 0,         -- Line clears for one piece
 		clear_cols = {},    -- Lines cleared (literal)
 
 		start_delay = 1.25, -- Ready? Go!
 
 		points = 0,         -- This game uses TA's hidden point system to award grades
 		point_decay = 0,
+		soft = 0,
 
 		topped_out = false, -- Game over
 		credits = false, -- In M roll
@@ -195,14 +363,70 @@ local function ResetPlayerState()
 		credit_fadeout = 0,
 		credit_time = 0,
 
-		history = { "Z", "Z", "Z", "Z" },
-		next_pieces = {},
+		history = { starting_piece, "Z", "Z", "Z" },
+		randomizer = randomizer,
+		next_pieces = next_pieces,
 		current_piece = {},
 		ghost_piece = {},
 
 		board = board,
 	}
+	return state
 end
 
--- Current state
-JtemTGM.PlayerState = ResetPlayerState()
+function JtemTGM.CreateCurrentPiece(x, y, piece, rot)
+	return {
+		x = x,
+		y = y,
+		piece = piece,
+		rotation = rot,
+	}
+end
+
+function JtemTGM.ChangeState(game, newstate)
+	-- dumb hack to make sure we dont go to are before placing the piece
+	if newstate == STATE_ARE and next(game.current_piece) then
+		game.state = STATE_LOCKING
+		newstate = STATE_LOCKED
+	end
+
+	if newstate == STATE_LOCKING then
+		game.lock_timer = game.lock_delay
+		game.clears = 0
+	end
+
+	if newstate == STATE_DROPPING then
+		if game.state == STATE_ARE then
+			if (game.level % 100) ~= 99 and game.level ~= MAX_LEVEL - 1 then
+				game.level = game.level + 1
+			end
+		end
+		if game.level >= MAX_LEVEL then
+			game.level = MAX_LEVEL
+			game.credits = true
+		end
+
+		if game.state == STATE_GO or game.state == STATE_ARE or game.state == STATE_INVISIBLETETRIS then
+			-- change piece
+			game.current_piece = JtemTGM.CreateCurrentPiece(3, -1, game.next_pieces[1], 1)
+			-- IRS
+			if love.keyboard.isDown("z") then
+
+			end
+			if love.keyboard.isDown("x") then
+
+			end
+			table.remove(game.next_pieces, 1)
+			table.insert(game.next_pieces, JtemTGM.GeneratePiece(game.randomizer))
+			-- TODO sound
+		end
+		game.clears = 0
+		game.soft = game.state == STATE_ARE and 0 or game.soft
+	end
+
+	if newstate == STATE_LOCKED and game.state == STATE_LOCKING then
+
+	end
+end
+
+--#endregion
