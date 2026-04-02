8aaaa7fa1d96c597354befd0a00c2ec46c05db4a
RandomsongV2 <145283437+RandomsongV2@users.noreply.github.com>
2026-04-02 05:17:29 +0000

    add team credits

---
+++ content/Team Shrug/main.lua
@@ -0,0 +1,53 @@
+-- [["¯\_(ツ)_/¯"]]
+--
+PotatoPatchUtils.Team({
+    name = "shrug", 
+    loc = true,
+    colour = HEX("B6334C"),
+    --colour = HEX("A65A60"), --low contrast variant
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "randomsongv2",
+    loc = true,
+    colour = HEX("7b1414"),
+    atlas = 'Joker',
+    pos = {x = 6, y = 8},
+    soul_pos = {x = 6, y = 9},
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "microwave",
+    loc = true,
+    colour = HEX("845aad"),
+    atlas = 'Joker',
+    pos = {x = 4, y = 8},
+    soul_pos = {x = 4, y = 9},
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "waffle",
+    loc = true,
+    colour = HEX("45ad71"),
+    atlas = 'Joker',
+    pos = {x = 7, y = 8},
+    soul_pos = {x = 7, y = 9},
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "atiredguy",
+    loc = true,
+    colour = HEX("8f8275"),
+    atlas = 'Joker',
+    pos = {x = 5, y = 8},
+    soul_pos = {x = 5, y = 9},
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "edwardrobinson",
+    loc = true,
+    colour = HEX("ffffff"),
+    atlas = 'Joker',
+    pos = {x = 3, y = 8},
+    soul_pos = {x = 3, y = 9},
+})
---
+++ localization/Team Shrug/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_shrug = {name = [[¯\_(ツ)_/¯]]}, -- todo add font for the smile and hands
+            PotatoPatchDev_randomsongv2 = {name = "RandomsongV2", text = {"made 0 songs", "wow!"}},
+            PotatoPatchDev_microwave = {name = "Microwave", text = {'loves to say "sweet"', "wow!"}},
+            PotatoPatchDev_waffle = {name = "waffle", text = {"doctor with PHD", "wow!"}},
+            PotatoPatchDev_atiredguy = {name = "A tired guy", text = {"seems very tired", "wow!"}},
+            PotatoPatchDev_edwardrobinson = {name = "Edward Robinson", text = {"cool name", "wow!"}},
+        },
+    },
+}
