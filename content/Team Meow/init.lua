7fca403419fc6a5cbf39d385f4501b0901c8fbfc
ThunderEdge <alvindong9913@gmail.com>
2026-04-02 16:44:53 -0500

    First commit

---
+++ content/Team Meow/init.lua
@@ -0,0 +1,63 @@
+PotatoPatchUtils.Team({
+	name = "meow",
+	colour = HEX("f9d0d1"),
+	loc = true,
+})
+
+local thunderedge_colour = SMODS.Gradient({
+	key = "thunderedge_gradient",
+	colours = {
+		HEX("89C41B"),
+		HEX("C5CC41"),
+	},
+	cycle = 1.5,
+})
+
+PotatoPatchUtils.Developer({
+	name = "thunderedge",
+    colour = thunderedge_colour,
+    loc = true,
+    team = "meow",
+})
+
+PotatoPatchUtils.Developer({
+	name = "corobo",
+    colour = G.C.UI.TEXT_LIGHT, -- Change this
+    loc = true,
+    team = "meow",
+})
+
+PotatoPatchUtils.Developer({
+	name = "revo",
+    colour = G.C.UI.TEXT_LIGHT, -- Change this
+    loc = true,
+    team = "meow",
+})
+
+PotatoPatchUtils.Developer({
+	name = "gappie",
+    colour = G.C.UI.TEXT_LIGHT, -- Change this
+    loc = true,
+    team = "meow",
+})
+
+PotatoPatchUtils.Developer({
+	name = "incognito",
+    colour = G.C.UI.TEXT_LIGHT, -- Change this
+    loc = true,
+    team = "meow",
+})
+
+PotatoPatchUtils.Developer({
+	name = "toma",
+    colour = G.C.UI.TEXT_LIGHT, -- Change this
+    loc = true,
+    team = "meow",
+})
+
+PotatoPatchUtils.Developer({
+	name = "silverautumn",
+    colour = G.C.UI.TEXT_LIGHT, -- Change this
+    loc = true,
+    team = "meow",
+})
---
+++ localization/Team Meow/en-us.lua
@@ -0,0 +1,56 @@
+return {
+	descriptions = {
+		PotatoPatch = {
+			PotatoPatchTeam_meow = { name = "Team Meow" },
+			PotatoPatchDev_thunderedge = {
+				name = "ThunderEdge",
+				text = {
+					{
+						"Programmer",
+					},
+					{
+						"Developer of {C:worm_thunderedge_gradient}Multiverse{} and {C:attention}JROK{}",
+						"Co-developer of {C:attention}Blind Expander{}",
+					},
+				},
+			},
+			PotatoPatchDev_corobo = {
+				name = "Corobo",
+				text = {
+					"Programmer",
+				},
+			},
+			PotatoPatchDev_revo = {
+				name = "Revo",
+				text = {
+					"Programmer",
+				},
+			},
+			PotatoPatchDev_gappie = {
+				name = "Gappie",
+				text = {
+					"Artist",
+				},
+			},
+			PotatoPatchDev_incognito = {
+				name = "Incognito",
+				text = {
+					"Programmer, Artist",
+				},
+			},
+			PotatoPatchDev_toma = {
+				name = "Tomatose",
+				text = {
+					"Programmer, Artist",
+				},
+			},
+			PotatoPatchDev_silverautumn = {
+				name = "SilverAutumn",
+				text = {
+					"Programmer, Artist",
+				},
+			},
+		},
+	},
+	misc = {},
+}
