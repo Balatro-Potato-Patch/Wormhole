671a53a725177cad7a43e0d5b616621c3ef5db9a
ThunderEdge <alvindong9913@gmail.com>
2026-04-02 18:01:07 -0500

    Update init.lua

--- content/Team Meow/init.lua
+++ content/Team Meow/init.lua
@@ -15,49 +15,49 @@ local thunderedge_colour = SMODS.Gradient({
 
 PotatoPatchUtils.Developer({
 	name = "thunderedge",
-    colour = thunderedge_colour,
-    loc = true,
-    team = "meow",
+	colour = thunderedge_colour,
+	loc = true,
+	team = "meow",
 })
 
 PotatoPatchUtils.Developer({
 	name = "corobo",
-    colour = G.C.UI.TEXT_LIGHT, -- Change this
-    loc = true,
-    team = "meow",
+	colour = G.C.GOLD, -- Change this
+	loc = true,
+	team = "meow",
 })
 
 PotatoPatchUtils.Developer({
 	name = "revo",
-    colour = HEX("7E7AFF"),
-    loc = true,
-    team = "meow",
+	colour = HEX("7E7AFF"),
+	loc = true,
+	team = "meow",
 })
 
 PotatoPatchUtils.Developer({
 	name = "gappie",
-    colour = G.C.UI.TEXT_LIGHT, -- Change this
-    loc = true,
-    team = "meow",
+	colour = G.C.UI.TEXT_LIGHT, -- Change this
+	loc = true,
+	team = "meow",
 })
 
 PotatoPatchUtils.Developer({
 	name = "incognito",
-    colour = G.C.UI.TEXT_LIGHT, -- Change this
-    loc = true,
-    team = "meow",
+	colour = HEX("D0D0D0"),
+	loc = true,
+	team = "meow",
 })
 
 PotatoPatchUtils.Developer({
 	name = "toma",
-    colour = G.C.UI.TEXT_LIGHT, -- Change this
-    loc = true,
-    team = "meow",
+	colour = HEX("FCB3EA"),
+	loc = true,
+	team = "meow",
 })
 
 PotatoPatchUtils.Developer({
 	name = "silverautumn",
-    colour = G.C.UI.TEXT_LIGHT, -- Change this
-    loc = true,
-    team = "meow",
+	colour = G.C.UI.TEXT_LIGHT, -- Change this
+	loc = true,
+	team = "meow",
 })
