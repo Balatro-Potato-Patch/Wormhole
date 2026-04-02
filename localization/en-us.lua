effd3b709523a15e6489c3538a7c91a0f94bb6ae
xFooping <144247190+xFooping@users.noreply.github.com>
2026-04-02 18:08:59 -0400

    Add localization descriptions for Joker characters

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,3 +1,37 @@
 return {
-    
+    descriptions = {
+        Joker = {
+            j_worm_the_past = {
+                name = "The Past",
+                text = {
+                    "Tracks total {C:attention}Hands{} played this run",
+                    "Currently: {C:attention}#1#",
+                    "{C:inactive}Yearns for the Future."
+                }
+            },
+            j_worm_the_past_paired = {
+                name = "The Past",
+                text = {
+                    "{C:attention}#1#{} hands played X {C:attention}#2#{} Jokers bought",
+                    "{C:inactive}They were never apart. Only waiting."
+                }
+            },
+            j_worm_the_future = {
+                name = "The Future",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "for each {C:attention}Joker{} purchased this run",
+                    "{C:inactive}Yearns for the Past."
+                }
+            },
+            j_worm_the_future_paired = {
+                name = "The Future",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "{C:attention}#2#{} hands played X {C:attention}#3#{} Jokers bought",
+                    "{C:inactive}They were never apart. Only waiting."
+                }
+            }
+        }
+    }
 }
