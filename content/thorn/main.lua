89fd8debe60c15daff36d460880281af0a839589
Evgast <136436468+Evgast@users.noreply.github.com>
2026-04-02 19:06:31 +1100

    Team object stuff

---
+++ content/thorn/main.lua
@@ -0,0 +1,50 @@
+
+PotatoPatchUtils.Team({
+    name = "thorn", 
+    loc = true,
+    colour = HEX("5ddaF3"),
+})
+PotatoPatchUtils.Developer({
+    team = "thorn",
+    name = "marie",
+    loc = true,
+    atlas = 'Joker',
+    pos = {x = 0, y = 0},
+    soul_pos = {x = 0, y = 0},
+})
+PotatoPatchUtils.Developer({
+    team = "thorn",
+    name = "willow",
+    loc = true,
+    atlas = 'Joker',
+    pos = {x = 0, y = 0},
+})
+PotatoPatchUtils.Developer({
+    team = "thorn",
+    name = "mtw",
+    loc = true,
+    atlas = 'Joker',
+    pos = {x = 0, y = 0},
+})
+PotatoPatchUtils.Developer({
+    team = "thorn",
+    name = "evgast",
+    colour = G.C.PURPLE,
+    loc = true,
+    atlas = 'Joker',
+    pos = {x = 0, y = 0},
+})
+PotatoPatchUtils.Developer({
+    team = "thorn",
+    name = "sophie",
+    loc = true,
+    atlas = 'Joker',
+    pos = {x = 0, y = 0},
+})
+PotatoPatchUtils.Developer({
+    team = "thorn",
+    name = "hatstack",
+    loc = true,
+    atlas = 'Joker',
+    pos = {x = 0, y = 0},
+})
---
+++ localization/thorn/en-us.lua
@@ -0,0 +1,53 @@
+return {
+    descriptions = {
+            PotatoPatch = {
+                PotatoPatchTeam_thorn = {
+                    name = "ThornTeam",
+                },
+                PotatoPatchDev_marie = {
+                    name = "Marie",
+                    text = {
+                        "Replace this please"
+                    },
+                },
+                PotatoPatchDev_willow = {
+                    name = "Willow",
+                    text = {
+                        "Replace this please"
+                    },
+                },
+                PotatoPatchDev_mtw = {
+                    name = "MightyKingWario",
+                    text = {
+                        "Replace this please"
+                    },
+                },
+                PotatoPatchDev_evgast = {
+                    name = "Evgast",
+                    text = {
+                        {
+                        "True Fact:",
+                        "The {C:red}government{} doesn't",
+                        "want you to play {C:green}Jimbo&!{}"
+                        },
+                        {
+                        "Fun Fact:",
+                        "Purple {C:purple}bitches{} are evil"
+                        }
+                    }
+                },
+                PotatoPatchDev_sophie = {
+                    name = "Sophie",
+                    text = {
+                        "Replace this please"
+                    },
+                },
+                PotatoPatchDev_hatstack = {
+                    name = "Hat Stack",
+                    text = {
+                        "Replace this please"
+                    },
+                }
+            }
+    },
+}
