54900add083527528cefe082545de337c7251484
VeryMicrowaveSafe <superhypercrazy@gmail.com>
2026-04-01 21:30:19 -0700

    Got started

---
+++ content/Team Shrug/1_main.lua
@@ -0,0 +1,47 @@
+-- Cards for Team Members
+SMODS.Atlas {
+    key = "atlas_microwave",
+    px = 71,
+    py = 95,
+    path = "shrug/dev_microwave.png"
+}
+
+
+-- [["¯\_(ツ)_/¯"]]
+PotatoPatchUtils.Team({
+    name = "shrug", -- todo add font for the smile
+    colour = HEX("B6334C"),
+    --colour = HEX("A65A60"), --low contrast variant
+    loc = "PotatoPatchTeam_Shrug"
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "randomsongv2",
+    colour = HEX("7b1414"),
+    loc = true
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "microwave",
+    atlas = "worm_atlas_microwave",
+    colour = HEX("845aad"),
+    loc = true
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "waffle",
+    colour = HEX("45ad71"),
+    loc = true
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "a_tired_guy",
+    colour = HEX("8f8275"),
+    loc = true
+})
+PotatoPatchUtils.Developer({
+    team = "shrug",
+    name = "edward_robinson",
+    colour = HEX("ffffff"),
+    loc = true
+})
---
+++ localization/shrug/en-us.lua
@@ -0,0 +1,69 @@
+return {
+
+    descriptions = {
+
+
+
+
+
+        ---POTATO PATCH---
+        ------------------
+        ---POTATO PATCH---
+        PotatoPatch = {
+            PotatoPatchTeam_Shrug = {name = [[{f:5}¯\_(ツ)_/¯{}]]},
+            PotatoPatchDev_microwave = {
+                name = "Microwave",
+                text = {
+                    "TEST TEST TEST"
+                }
+            },
+            PotatoPatchDev_randomsongv2 = {
+                name = "RandomsongV2",
+                text = {
+                    "TEST TEST TEST"
+                }
+            },
+            PotatoPatchDev_waffle = {
+                name = "Waffle",
+                text = {
+                    "TEST TEST TEST"
+                }
+            },
+            PotatoPatchDev_a_tired_guy = {
+                name = "A Tired Guy",
+                text = {
+                    "TEST TEST TEST"
+                }
+            },
+            PotatoPatchDev_edward_robinson = {
+                name = "Edward Robinson",
+                text = {
+                    "TEST TEST TEST"
+                }
+            },
+        },
+
+
+
+
+        ---JOKERS---
+        ------------
+        ---JOKERS---
+        Joker = {
+
+
+            -- SPACEWALK
+            j_worm_shrug_spacewalk = {
+                name = "Spacewalk",
+                text = {
+                    "Creates a free",
+                    "{C:planet}Meteor Tag{} at the",
+                    "{C:attention}end of round{} if no",
+                    "{C:planet}Planet{} cards were used",
+                    "since last round",
+                }
+            }
+        },
+    }
+
+}
