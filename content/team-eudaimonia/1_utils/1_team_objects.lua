4c0d48540de54d6c32e7c570205d8c79231ef706
LasagnaFelidae <entitesucrerie@gmail.com>
2026-04-02 03:02:57 +0200

    added loc files and folder bs + team/dev objects

---
+++ content/team-eudaimonia/1_utils/1_team_objects.lua
@@ -0,0 +1,99 @@
+SMODS.Atlas {
+	key = "eud_teamFeli",
+	path = "team-eudaimonia/teamcards/feli.png",
+	px = 71,
+	py = 95,
+}
+
+PotatoPatchUtils.Team({
+    name = "TeamEudaimonia",
+    colour = HEX('757bc8'),
+    loc = true,
+}) 
+
+PotatoPatchUtils.Developer({
+    name = "LasagnaFelidae",
+    colour = G.C.EDITION,
+    loc = true,
+    team = "TeamEudaimonia",
+    atlas = "worm_eud_teamFeli",
+    pos = {x=0,y=0},
+})
+
+PotatoPatchUtils.Developer({
+    name = "Plasma",
+    colour = HEX('baa1db'),
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamPlasma"
+    --pos = {x=0,y=0}
+})
+
+PotatoPatchUtils.Developer({
+    name = "soulware",
+    colour = HEX('00ff00'),
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamSoul"
+    --pos = {x=0,y=0}
+})
+PotatoPatchUtils.Developer({
+    name = "TigerTHawk",
+    colour = HEX('FF00D0'),
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamTiger"
+    --pos = {x=0,y=0}
+})
+
+PotatoPatchUtils.Developer({
+    name = "iamarta",
+    colour = HEX('FFBC57'),
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamArta"
+    --pos = {x=0,y=0}
+})
+PotatoPatchUtils.Developer({
+    name = "m0x3s",
+    colour = HEX('9d39c4'),
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamM0x3s"
+    --pos = {x=0,y=0}
+})
+PotatoPatchUtils.Developer({
+    name = "Sillyz",
+    colour = G.C.PURPLE,
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamSillyz"
+    --pos = {x=0,y=0}
+})
+PotatoPatchUtils.Developer({
+    name = "Hunter",
+    colour = G.C.SECONDARY_SET.Planet,
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamHunter"
+    --pos = {x=0,y=0}
+})
+
+PotatoPatchUtils.Developer({
+    name = "Typ0",
+    colour = SMODS.Gradient{key = "Typ0", colours = {HEX('A4C2F4'), HEX('a4eaf4')},cycle = 8},
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamTyp0"
+    --pos = {x=0,y=0}
+})
+
+PotatoPatchUtils.Developer({
+    name = "Jewel",
+    colour = HEX('F399FF'),
+    loc = true,
+    team = "TeamEudaimonia",
+    --atlas_key = "eud_teamJewel"
+    --pos = {x=0,y=0}
+})
+
---
+++ localization/team-eudaimonia/en-us.lua
@@ -0,0 +1,53 @@
+return {
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_TeamEudaimonia = {
+                name = "Team Eudaimonia",
+            },
+
+            PotatoPatchDev_LasagnaFelidae = {
+                name = "LasagnaFelidae",
+                text = {"big fat hairy deal"}
+            },
+            PotatoPatchDev_Plasma = {
+                name = "Plasma",
+                text = {"."}
+            },
+            PotatoPatchDev_soulware = {
+                name = "soulware",
+                text = {"."}
+            },
+            PotatoPatchDev_TigerTHawk = {
+                name = "TigerTHawk",
+                text = {"."}
+            },
+            PotatoPatchDev_iamarta = {
+                name = "iamarta",
+                text = {"."}
+            },
+            PotatoPatchDev_m0x3s = {
+                name = "m0x3s",
+                text = {"."}
+            },
+            PotatoPatchDev_Hunter = {
+                name = "ARandomHunter",
+                text = {"."}
+            },
+            PotatoPatchDev_Sillyz = {
+                name = "Sillyz",
+                text = {"."}
+            },
+            PotatoPatchDev_Typ0 = {
+                name = "Typ0",
+                text = {"."}
+            },
+            PotatoPatchDev_Jewel = {
+                name = "Jewel",
+                text = {"."}
+            },
+
+
+        },
+
+    }
+}
