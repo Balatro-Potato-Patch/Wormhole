3d8fc0bfc8bb63f6246ab386079b272b6afd129c
Opal <183864149+OOkayOak@users.noreply.github.com>
2026-04-03 19:06:23 +0100

    Adds a file for the Team and Developers, + localization file

---
+++ content/acme_corp/1 Priority/team_devs.lua
@@ -0,0 +1,7 @@
+PotatoPatchUtils.LOC.process_loc_text('test.lua')
+
+PotatoPatchUtils.Team {
+    name = 'ACME',
+    colour = HEX('fb7025'),
+    loc = true
+}
---
+++ localization/acme_corp/en-us.lua
@@ -0,0 +1,9 @@
+return{
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_ACME = {
+                name = 'ACME Corporation'
+            },
+        }
+    }
+}
