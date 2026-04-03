b42b090b2c20506898b6c36300680ab385b6093b
xFooping <144247190+xFooping@users.noreply.github.com>
2026-04-03 12:30:57 -0400

    Adjusted files to meet SJJ requirements

--- localization/Riverboat/en-us.lua
+++ localization/Riverboat/en-us.lua
@@ -6,7 +6,48 @@ return {
             PotatoPatchDev_fooping = { name = "fooping", text = { "t" } },
         },
         Joker = {
-            worm_riverboat_test = {
+            j_worm_the_past = {
+                name = "The Past",
+                text = {
+                    "Tracks total {C:attention}Hands{} played this run",
+                    "Currently: {C:attention}#1#",
+                    "{C:inactive}Yearns for the Future."
+                }
+            },
+            j_worm_the_past_paired = {
+                name = "The Past",
+                text = {
+                    "{C:attention}#1#{} hands played X {C:attention}#2#{} Jokers bought",
+                    "{C:inactive}They were never apart. Only waiting."
+                }
+            },
+            j_worm_the_future = {
+                name = "The Future",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "for each {C:attention}Joker{} purchased this run",
+                    "{C:inactive}Yearns for the Past."
+                }
+            },
+            j_worm_the_future_paired = {
+                name = "The Future",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "{C:attention}#2#{} hands played X {C:attention}#3#{} Jokers bought",
+                    "{C:inactive}They were never apart. Only waiting."
+                }
+            },
+            j_worm_ulala = {
+                name = "Ulala",
+                text = {
+                    "Gains {C:mult}+#2#{} Mult if played hand",
+                    "contains a scoring {C:attention}5{},",
+                    "resets if played hand does not",
+                    "contain a scoring {C:attention}5{}",
+                    "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)"
+                }
+            },
+            j_worm_riverboat_test = {
                 name = "test joker",
                 text = {
                     "When a {C:planet}Planet{} card is used, add",
