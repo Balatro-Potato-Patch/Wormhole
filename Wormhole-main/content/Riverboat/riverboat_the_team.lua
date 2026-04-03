3dd3ae0a3a2fb907e36e0b67b8e797d3e6d027cd
xFooping <144247190+xFooping@users.noreply.github.com>
2026-04-03 12:25:54 -0400

    Merged everything to fit the SJJ requirements

---
+++ Wormhole-main/content/Riverboat/riverboat_the_team.lua
@@ -0,0 +1,17 @@
+PotatoPatchUtils.Team {
+    name = "riverboat",
+    colour = HEX("ff8c69"),
+    loc = true,
+}
+
+PotatoPatchUtils.Developer {
+    name = "blamperer",
+    team = "riverboat",
+    loc = true
+}
+
+PotatoPatchUtils.Developer {
+    name = "fooping",
+    team = "riverboat",
+    loc = true
+}
---
+++ Wormhole-main/localization/Riverboat/en-us.lua
@@ -0,0 +1,60 @@
+return {
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchTeam_riverboat = { name = "Riverboat" },
+            PotatoPatchDev_blamperer = { name = "blamperer", text = { "t" } },
+            PotatoPatchDev_fooping = { name = "fooping", text = { "t" } },
+        },
+        Joker = {
+            j_worm_the_past = {
+                name = "The Past",
+                text = {
+                    "Tracks total {C:attention}Hands{} played this run",
+                    "Currently: {C:attention}#1#",
+                    "{C:inactive}Yearns for the Future."
+                }
+            },
+            j_worm_the_past_paired = {
+                name = "The Past",
+                text = {
+                    "{C:attention}#1#{} hands played X {C:attention}#2#{} Jokers bought",
+                    "{C:inactive}They were never apart. Only waiting."
+                }
+            },
+            j_worm_the_future = {
+                name = "The Future",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "for each {C:attention}Joker{} purchased this run",
+                    "{C:inactive}Yearns for the Past."
+                }
+            },
+            j_worm_the_future_paired = {
+                name = "The Future",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "{C:attention}#2#{} hands played X {C:attention}#3#{} Jokers bought",
+                    "{C:inactive}They were never apart. Only waiting."
+                }
+            },
+            j_worm_ulala = {
+                name = "Ulala",
+                text = {
+                    "Gains {C:mult}+#2#{} Mult if played hand",
+                    "contains a scoring {C:attention}5{},",
+                    "resets if played hand does not",
+                    "contain a scoring {C:attention}5{}",
+                    "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)"
+                }
+            },
+            j_worm_riverboat_test = {
+                name = "test joker",
+                text = {
+                    "When a {C:planet}Planet{} card is used, add",
+                    "{X:purple,C:white}#1#X{} the product of its {C:chips}Chips{} and {C:mult}Mult",
+                    "to {C:purple}Score{} immediately"
+                }
+            }
+        }
+    }
+}
