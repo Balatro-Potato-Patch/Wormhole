fdb41df0786ac589fa5babcd729ebf67c12db6d2
blamperer <137224671+blamperer@users.noreply.github.com>
2026-04-02 20:09:57 -0400

    oops little a merge conflict

--- localization/Riverboat/en-us.lua
+++ localization/Riverboat/en-us.lua
@@ -4,6 +4,16 @@ return {
             PotatoPatchTeam_riverboat = { name = "Riverboat" },
             PotatoPatchDev_blamperer = { name = "blamperer", text = { "t" } },
             PotatoPatchDev_fooping = { name = "fooping", text = { "t" } },
+        },
+        Joker = {
+            worm_riverboat_test = {
+                name = "test joker",
+                text = {
+                    "When a {C:planet}Planet{} card is used, add",
+                    "{X:purple,C:white}10X{} the product of its {C:chips}Chips{} and {C:mult}Mult",
+                    "to {C:purple}Score{} immediately"
+                }
+            }
         }
     }
 }
