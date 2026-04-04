ea181415851e857aca2e8b22aa6680a777ddef70
Eris (borb) <remcosveen@gmail.com>
2026-04-04 13:01:54 +0200

    team file

---
+++ content/awesomeswagmoney/00_team.lua
@@ -0,0 +1,29 @@
+PotatoPatchUtils.Team{
+    name = "Team Awesomeswagmoney",
+    colour = HEX("345678")
+}
+
+PotatoPatchUtils.Developer{
+    name = "Garb",
+    team = "Team Awesomeswagmoney",
+}
+
+PotatoPatchUtils.Developer{
+    name = "Poker The Poker",
+    team = "Team Awesomeswagmoney",
+}
+
+PotatoPatchUtils.Developer{
+    name = "Omega",
+    team = "Team Awesomeswagmoney",
+}
+
+PotatoPatchUtils.Developer{
+    name = "Superb Thing",
+    team = "Team Awesomeswagmoney",
+}
+
+PotatoPatchUtils.Developer{
+    name = "Eris",
+    team = "Team Awesomeswagmoney",
+}
