81f151b38a90f80c838cf699e0028ee6402e146b
youhwithanh <lipegrs+5@gmail.com>
2026-04-04 23:05:49 -0300

    Big Brother + atlas and assets setup

---
+++ content/acme_corp/1 Priority/atlas.lua
@@ -0,0 +1,25 @@
+SMODS.Atlas {
+	key = 'ACME_jokers',
+	path = 'acme-corp/ACME_jokers.png',
+	px = 71,
+	py = 95
+}
+--[[ SMODS.Atlas {
+	key = 'ACME_gadgets',
+	path = 'acme-corp/ACME_gadgets.png',
+	px = 71,
+	py = 95
+}
+SMODS.Atlas {
+	key = 'ACME_backs',
+	path = 'acme-corp/ACME_backs.png',
+	px = 71,
+	py = 95
+}
+]]
+SMODS.Atlas {
+	key = 'ACME_boosters',
+	path = 'acme-corp/ACME_boosters.png',
+	px = 71,
+	py = 95
+}
