cdf854b9d12a6863ea6bc582447c91342ccf5b85
cosmeggo <cosmeggo@gmail.com>
2026-04-05 20:11:36 +0100

    Added personal localisations

--- localization/team-eudaimonia/en-us.lua
+++ localization/team-eudaimonia/en-us.lua
@@ -9,9 +9,9 @@ return {
                 name = "LasagnaFelidae",
                 text = {"big fat hairy deal"}
             },
-            PotatoPatchDev_cosmeggo = {
-                name = "Cosmeggo",
-                text = {"."}
+            PotatoPatchDev_Plasma = {
+                name = "cosmeggo",
+                text = {"Formerly Plasma!"}
             },
             PotatoPatchDev_soulware = {
                 name = "soulware",
@@ -25,9 +25,9 @@ return {
                 name = "iamarta",
                 text = {"bonjour"}
             },
-            PotatoPatchDev_M0xes = {
-                name = "M0xes",
-                text = {"Me."}
+            PotatoPatchDev_m0x3s = {
+                name = "m0x3s",
+                text = {"."}
             },
             PotatoPatchDev_Hunter = {
                 name = "ARandomHunter",
@@ -39,7 +39,7 @@ return {
             },
             PotatoPatchDev_Typ0 = {
                 name = "Typ0",
-                text = {"Hit Game Dev Right Here"}
+                text = {"."}
             },
             PotatoPatchDev_Jewel = {
                 name = "Jewel",
@@ -49,68 +49,36 @@ return {
 
         },
         Joker = {
-            j_worm_euda_wowsignal = {
-                name = "Wow! Signal",
+            j_worm_message = {
+                name = {
+                    "The Message",
+                },
                 text = {
-                    "{C:green}#1# in #2#{} chance to give ",
-                    "{X:chips,C:white}X#3#-#4#{} Chips otherwise,",
-                    "give {C:chips}#5#-#6#{} Chips",
-                },
-                
+                    {
+                    "{X:red,C:white}X#1#{} Mult",
+                    "{C:green}#2# in #3#{} chance to",
+                    "receive an {C:inactive}answer{}"
+                    },
+                    {
+                    "{C:red}Use with caution.{}"
+                    }
+                }
             },
-            j_worm_jokecolony = {
-                name = "Joke Colony",
+            j_worm_answer = {
+                name = {
+                    "The Answer",
+                },
                 text = {
-                    "Store Jokers in a {C:attention}Colony",
-                    "Gives {C:mult}+#1#{} Mult per Joker in the {C:attention}Colony",
-                    "{C:inactive}(Population #2# for {C:red}+#3#{C:inactive} Mult)",
-                },
+                    {
+                    "All {C:attention}Boss Blinds{}",
+                    "are replaced with",
+                    "{C:inactive}The Anomaly{}"
+                    },
+                    {
+                    "{C:inactive}You should have listened...{}",
+                    }
+                }
             },
-            j_worm_euda_rocket = {
-                name = "Space Shuttle",
-                text = {
-                    "{C:red}+#1#{} Mult?",
-                    "{C:inactive}(Blast off in: #3#)"
-                },
-            },
-			j_worm_euda_rocket_a = {
-                name = "Space Shuttle",
-                text = {
-					{
-                    "{C:red}+#1#{} Mult",
-					},
-					{
-					"Increase Mult by {C:red}#2#{}",
-					"if the {C:attention}scored hand{} isn't",
-					"the most played {C:attention}poker hand"
-					},
-                },
-            },
-        },
-    },
-    misc = {
-            poker_hand_descriptions = {
-                worm_euda_pkr_wow = {
-                    "Play a 6, a 3, a Queen, a Jack, and a 5",
-                },
-                worm_euda_pkr_wow_f = {
-                        "Play a 6, a 3, a Queen, a Jack, and a 5",
-                        "with all cards sharing the same suit",
-                },
-            },
-            poker_hands = {
-                worm_pkr_wow = "Wow!",
-                worm_pkr_wow_f = "Wow! Flush"
-            },
-            dictionary = {
-                k_worm_jokecolony_ship_button = "SHIP",
-                k_worm_jokecolony_receive_button = "RELEASE",
-                k_worm_specificcolony_ship_button = "Ship Jokers",
-                k_worm_specificcolony_receive_button = "Release Jokers",
-                k_worm_specificcolony_ship_title = "Sending to Colony ",
-                k_worm_specificcolony_receive_title = "Release from Colony ",
-                worm_euda_pkr_wow = "Wow!",
-                worm_euda_pkr_wow_f = "Wow! Flush",
-            }
-        },
+        }
+    }
 }
