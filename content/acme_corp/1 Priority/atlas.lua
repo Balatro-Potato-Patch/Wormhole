087e2725b7ebb8e64b72df2f59efc78debe455a3
Juniper <basil.squared1@gmail.com>
2026-04-06 06:17:12 -0500

    resolve merge bullshit. ohmy god

--- content/acme_corp/1 Priority/atlas.lua
+++ content/acme_corp/1 Priority/atlas.lua
@@ -18,7 +18,15 @@ SMODS.Atlas {
 	py = 95
 }
 ]]
+
+Atlas_AcmeCredits = SMODS.Atlas {
+	key = 'ACME_credits',
+	path = 'acme_corp/ACME_credits.png',
+	px = 71,
+	py = 95
+}
 Atlas_AcmeBoosters = SMODS.Atlas {
+
 	key = 'ACME_boosters',
 	path = 'acme_corp/ACME_boosters.png',
 	px = 71,
