7f261581a2ad8a15c674dac8648c807c036a74ed
WombatCountry <53626867+wombatCountry@users.noreply.github.com>
2026-04-06 13:42:12 -0400

    added booster pack entries

--- localization/Hedonia/en-us.lua
+++ localization/Hedonia/en-us.lua
@@ -1,7 +1,7 @@
 -- I (wombat) used a lot of VanillaRemade as reference throughout this entire file https://github.com/nh6574/VanillaRemade/blob/main/src/tarots.lua https://github.com/nh6574/VanillaRemade/blob/main/localization/en-us.lua
 return {
     descriptions = {
-        wormhole_hedonia_menu={
+        worm_hedonia_menu={
             c_worm_template = {
                 name = 'The Sungwon Special',
                 text = {
@@ -9,11 +9,37 @@ return {
                 }
             }
         },
+        Other = {
+            p_worm_hedonia_menu_normal = {
+                name = "Menu Pack",
+                text = {
+                    "order {C:attention}#1#{} of up to",
+                    "{C:attention}#2#{} Menu items to",
+                    "be used immediately"
+                }
+            },
+            p_worm_hedonia_menu_jumbo = {
+                name = "Jumbo Menu Pack",
+                text = {
+                    "order {C:attention}#1#{} of up to",
+                    "{C:attention}#2#{} Menu items to",
+                    "be used immediately"
+                }
+            },
+            p_worm_hedonia_menu_mega = {
+                name = "Mega Menu Pack",
+                text = {
+                    "order {C:attention}#1#{} of up to",
+                    "{C:attention}#2#{} Menu items to",
+                    "be used immediately"
+                }
+            }
+        }
     },
     misc = {
         dictionary={
-            b_wormhole_hedonia_menu_cards = "Tonight's Menu",
-            k_wormhole_hedonia_menu = "Menu Item",
+            b_worm_hedonia_menu_cards = "Tonight's Menu",
+            k_worm_hedonia_menu = "Menu Item",
         },
     },
 }
