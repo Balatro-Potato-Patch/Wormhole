f0c7c7e3a46b493d4dcc4d928bc75c1bc3130b9c
youhwithanh <lipegrs+5@gmail.com>
2026-04-06 19:05:13 -0300

    i am the patcher i patch the asset names yes i am the G.E_manager:add_event() user

--- content/acme_corp/3 Main/Consumables/ball.lua
+++ content/acme_corp/3 Main/Consumables/ball.lua
@@ -30,4 +30,55 @@ SMODS.Consumable{
     atlas = 'ACME_gadgets',
     pos = {x=0, y=0},
     soul_pos = {x=0, y=1, draw = function(self, scale_mod) Wormhole.ACME.ball_function(self, scale_mod) end},
+	use = function(self, card, area, copier)
+        local used = copier or card
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                play_sound('tarot1')
+                used:juice_up(0.3, 0.5)
+                return true
+            end
+        }))
+        for i = 1, #G.hand.cards do
+            local percent = 1.15 - (i - 0.999) / (#G.hand.cards - 0.998) * 0.3
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15,
+                func = function()
+                    G.hand.cards[i]:flip()
+                    play_sound('card1', percent)
+                    G.hand.cards[i]:juice_up(0.3, 0.3)
+                    return true
+                end
+            }))
+        end
+        for i = 1, #G.hand.cards do
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    local _card = G.hand.cards[i]
+                    _card:set_ability(SMODS.poll_enhancement {key = 'ACME_ball', guaranteed = true})
+                    return true
+                end
+            }))
+        end
+        for i = 1, #G.hand.cards do
+            local percent = 0.85 + (i - 0.999) / (#G.hand.cards - 0.998) * 0.3
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15,
+                func = function()
+                    G.hand.cards[i]:flip()
+                    play_sound('tarot2', percent, 0.6)
+                    G.hand.cards[i]:juice_up(0.3, 0.3)
+                    return true
+                end
+            }))
+        end
+        delay(0.5)
+    end,
+    can_use = function(self, card)
+        return G.hand and #G.hand.cards > 0  
+    end,
 }
