88cbd6ec9f67797967ce17d0b9aa38f27c26d12c
Maelmc <30575979+Maelmc@users.noreply.github.com>
2026-04-07 00:30:30 +0200

    this saves some calculation time

--- content/JuryRigged/4_credits/1_team.lua
+++ content/JuryRigged/4_credits/1_team.lua
@@ -8,11 +8,13 @@ PotatoPatchUtils.Team({
       G.GAME.jr.curr_hand = context.scoring_name
     end
 
-    for _, v in pairs(Wormhole.JR_UTILS.Satellites) do
-      local _hand = Wormhole.JR_UTILS.get_hand(v.name)
-      if G.GAME.jr.curr_hand and G.GAME.jr.curr_hand == _hand and (G.GAME.jr.satellite_hands[_hand].level > 0) then
-        --print("TRIGERRED " .. v.name)
-        return v.calculate(self,context,v.vars)
+    if G.GAME.jr.curr_hand then
+      for _, v in pairs(Wormhole.JR_UTILS.Satellites) do
+        local _hand = Wormhole.JR_UTILS.get_hand(v.name)
+        if G.GAME.jr.curr_hand == _hand and (G.GAME.jr.satellite_hands[_hand].level > 0) then
+          --print("TRIGERRED " .. v.name)
+          return v.calculate(self,context,v.vars)
+        end
       end
     end
 
