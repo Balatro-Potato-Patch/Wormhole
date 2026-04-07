3c9d7a6d580fb3ab4928f331878557471884befd
cosmeggo <cosmeggo@gmail.com>
2026-04-08 00:02:13 +0100

    Add files via upload

--- localization/team-eudaimonia/en-us.lua
+++ localization/team-eudaimonia/en-us.lua
@@ -9,7 +9,7 @@ return {
                 name = "LasagnaFelidae",
                 text = {"big fat hairy deal"}
             },
-            PotatoPatchDev_Plasma = {
+            PotatoPatchDev_cosmeggo = {
                 name = "cosmeggo",
                 text = {"Formerly Plasma!"}
             },
@@ -25,9 +25,9 @@ return {
                 name = "iamarta",
                 text = {"bonjour"}
             },
-            PotatoPatchDev_m0x3s = {
-                name = "m0x3s",
-                text = {"."}
+            PotatoPatchDev_M0xes = {
+                name = "M0xes",
+                text = {"Me."}
             },
             PotatoPatchDev_Hunter = {
                 name = "ARandomHunter",
@@ -39,7 +39,7 @@ return {
             },
             PotatoPatchDev_Typ0 = {
                 name = "Typ0",
-                text = {"."}
+                text = {"Hit Game Dev Right Here","{s:0.75,C:default}Sprite By Inky{}"}
             },
             PotatoPatchDev_Jewel = {
                 name = "Jewel",
@@ -47,18 +47,6 @@ return {
             },
 
 
-        },
-        Blind = {
-            bl_worm_anomaly = {
-                name = {
-                    "The Anomaly"
-                },
-                text = {
-                    "When a hand played",
-                    "the blind is increased",
-                    "by #1#%"
-                }
-            }
         },
         Joker = {
             j_worm_message = {
@@ -90,7 +78,199 @@ return {
                     "{C:inactive}You should have listened...{}",
                     }
                 }
+            },j_worm_euda_wowsignal = {
+                name = "Wow! Signal",
+                text = {
+                    "{C:green}#1# in #2#{} chance to give ",
+                    "{X:chips,C:white}X#3#-#4#{} Chips otherwise,",
+                    "give {C:chips}#5#-#6#{} Chips",
+                },
+                
             },
-        }
-    }
+            j_worm_jokecolony = {
+                name = "Joke Colony",
+                text = {
+                    "Store Jokers in a {C:attention}Colony",
+                    "Gives {C:mult}+#1#{} Mult per Joker in the {C:attention}Colony",
+                    "{C:inactive}(Population #2# for {C:red}+#3#{C:inactive} Mult)",
+                },
+            },
+            j_worm_darkside = {
+                name = "Dark Side of the Moon",
+                text = {
+                    "{C:spades}#1#{} and {C:clubs}#2#{}",
+                    "are always drawn first",
+                },
+            },
+            j_worm_lunarcheese = {
+                name = "Lunar Cheese",
+                text = {
+                    "When sold, debuffs the {C:attention}Joker",
+                    "to its right for #1# rounds",
+                    "It {C:attention}permanently{} gains a retrigger"
+                },
+            },
+            j_worm_euda_rocket = {
+                name = "Space Shuttle",
+                text = {
+                    "{C:red}+#1#{} Mult?",
+                    "{C:inactive}(Blast off in: #3#)"
+                },
+            },
+			j_worm_euda_rocket_a = {
+                name = "Space Shuttle",
+                text = {
+					{
+                    "{C:red}+#1#{} Mult",
+					},
+					{
+					"Increase Mult by {C:red}#2#{}",
+					"if the {C:attention}scored hand{} isn't",
+					"the most played {C:attention}poker hand"
+					},
+                },
+            },
+			j_worm_bitflip = {
+                name = "Cosmic Bitflip",
+                text = {
+                    {
+                        "Gives either {C:chips}#4# Chips{} or",
+                        "{C:mult}#5#-#6# Mult{} depending on",
+                        "if the bit is flipped.",
+                        "{C:green}#1# in #2#{} chance to flip bits"
+                    },
+                    {
+                        "Bit.Flipped() == {C:attention}#3#;{}"
+                    }
+                }
+            }
+        },
+        Planet = {
+            c_worm_bigear = {
+                name = "Big Ear",
+                text = {
+                    "Level up",
+                    "{C:attention}Wow! and Wow! Flush{}",
+                    "By one Level"
+                },
+            }
+        },
+        Fate = {
+            c_worm_crunch = {
+                name = {"Big Crunch"},
+                text = {
+                    "Sets each card in {C:attention}deck",
+                    "to {C:attention}1{} of {C:attention}2{} random {C:attention}suits"
+                }
+            },
+            c_worm_freeze = {
+                name = {"Big Freeze"},
+                text = {
+                    "Sets each card in {C:attention}deck",
+                    "to {C:attention}1{} of {C:attention}4{} random {C:attention}ranks"
+                }
+            },
+            c_worm_rip = {
+                name = {"Big Rip"},
+                text = {
+                    "{C:attention}Duplicates{} each card in {C:attention}deck",
+                    "and {C:attention}halves{} all their {C:attention}ranks",
+                    "{C:inactive}(Rounded down)"
+                }
+            },
+            c_worm_slurp = {
+                name = {"Big Slurp"},
+                text = {
+                    "Randomly {C:attention}destroys{} half",
+                    "of the {C:attention}deck",
+                    "{C:inactive}(Rounded up)"
+                }
+            },
+            c_worm_bounce = {
+                name = {"Big Bounce"},
+                text = {
+                    "{C:attention}Destroys{} all cards in",
+                    "deck and {C:attention}replaces{} them with",
+                    "one card of each {C:attention}rank{} and {C:attention}suit"
+                }
+            }
+        },
+        Spectral = {
+            c_worm_bang = {
+                name = {"The Big Bang"},
+                text = {
+                    "{C:attention}Replaces{} all cards in deck",
+                    "with {C:attention,E:2}randomized{} ones",
+                    "{C:inactive}(Random suit, rank, enhancement, edition, seal)"
+                }
+            }
+        },
+        Other = {
+            p_worm_avadon_normal = {
+                name = "Avadon Pack",
+                text = {
+                    "Choose {C:attention}#1#{} of up to",
+                    "{C:attention}#2#{C:fate} Fate{} cards to",
+                    "be used immediately"
+                },
+            },
+            p_worm_avadon_jumbo = {
+                name = "Jumbo Avadon Pack",
+                text = {
+                    "Choose {C:attention}#1#{} of up to",
+                    "{C:attention}#2#{C:fate} Fate{} cards to",
+                    "be used immediately"
+                },
+            },
+            p_worm_avadon_mega = {
+                name = "Mega Avadon Pack",
+                text = {
+                    "Choose {C:attention}#1#{} of up to",
+                    "{C:attention}#2#{C:fate} Fate{} cards to",
+                    "be used immediately"
+                },
+            },
+            undiscovered_fate = {
+                name = "Not Discovered",
+                text = {
+                    "Purchase or use",
+                    "this card in an",
+                    "unseeded run to",
+                    "learn what it does",
+                },
+            },
+        },
+    },
+    misc = {
+            poker_hand_descriptions = {
+                worm_euda_pkr_wow = {
+                    "Play a 6, a 3, a Queen, a Jack, and a 5",
+                },
+                worm_euda_pkr_wow_f = {
+                        "Play a 6, a 3, a Queen, a Jack, and a 5",
+                        "with all cards sharing the same suit",
+                },
+			 	worm_pkr_pyramid = {
+                    "[NO_TEXT_DATA_DETECTED]",
+                },
+            },
+            poker_hands = {
+                worm_euda_pkr_wow = "Wow!",
+                worm_euda_pkr_wow_f = "Wow! Flush",
+            },
+            dictionary = {
+                k_worm_jokecolony_ship_button = "SHIP",
+                k_worm_jokecolony_receive_button = "RELEASE",
+                k_worm_specificcolony_ship_button = "Ship Jokers",
+                k_worm_specificcolony_receive_button = "Release Jokers",
+                k_worm_specificcolony_ship_title = "Sending to Colony ",
+                k_worm_specificcolony_receive_title = "Release from Colony ",
+
+                k_fate = "Fate",
+                b_fate_cards = "Fate Cards",
+                k_avadon_pack = "Avadon Pack",
+                k_worm_lunarcheese_message = "Cheesy!",
+            }
+        },
 }
+
