e77e43f32dcc028a054375620456e5c2be4237e2
cosmeggo <cosmeggo@gmail.com>
2026-04-08 00:02:55 +0100

    Add files via upload

--- content/team-eudaimonia/1_utils/1_team_objects.lua
+++ content/team-eudaimonia/1_utils/1_team_objects.lua
@@ -5,6 +5,25 @@ SMODS.Atlas {
 	py = 95,
 }
 
+SMODS.Atlas {
+	key = "eud_teamArta",
+	path = "team-eudaimonia/teamcards/arta.png",
+	px = 71,
+	py = 95,
+}
+SMODS.Atlas {
+	key = "eud_teamM0xes",
+	path = "team-eudaimonia/teamcards/M0xes.png",
+	px = 71,
+	py = 95,
+}
+SMODS.Atlas {
+	key = "eud_teamTyp0",
+	path = "team-eudaimonia/teamcards/Typ0.png",
+	px = 71,
+	py = 95,
+}
+
 PotatoPatchUtils.Team({
     name = "TeamEudaimonia",
     colour = HEX('757bc8'),
@@ -21,11 +40,11 @@ PotatoPatchUtils.Developer({
 })
 
 PotatoPatchUtils.Developer({
-    name = "Plasma",
+    name = "cosmeggo",
     colour = HEX('baa1db'),
     loc = true,
     team = "TeamEudaimonia",
-    --atlas_key = "eud_teamPlasma"
+    --atlas_key = "eud_teamCosmeggo"
     --pos = {x=0,y=0}
 })
 
@@ -51,16 +70,16 @@ PotatoPatchUtils.Developer({
     colour = HEX('FFBC57'),
     loc = true,
     team = "TeamEudaimonia",
-    --atlas_key = "eud_teamArta"
-    --pos = {x=0,y=0}
+    atlas = "worm_eud_teamArta",
+    pos = {x=0,y=0}
 })
 PotatoPatchUtils.Developer({
-    name = "m0x3s",
+    name = "M0xes",
     colour = HEX('9d39c4'),
     loc = true,
     team = "TeamEudaimonia",
-    --atlas_key = "eud_teamM0x3s"
-    --pos = {x=0,y=0}
+    atlas = "worm_eud_teamM0xes",
+    pos = {x=0,y=0}
 })
 PotatoPatchUtils.Developer({
     name = "Sillyz",
@@ -84,8 +103,8 @@ PotatoPatchUtils.Developer({
     colour = SMODS.Gradient{key = "Typ0", colours = {HEX('A4C2F4'), HEX('a4eaf4')},cycle = 8},
     loc = true,
     team = "TeamEudaimonia",
-    --atlas_key = "eud_teamTyp0"
-    --pos = {x=0,y=0}
+    atlas = "worm_eud_teamTyp0",
+    pos = {x=0,y=0},
 })
 
 PotatoPatchUtils.Developer({
