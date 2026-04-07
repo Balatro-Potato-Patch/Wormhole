5194573d40dde9c6873b2c2363c8c527693dec4c
cosmeggo <cosmeggo@gmail.com>
2026-04-08 00:03:45 +0100

    Add files via upload

--- content/team-eudaimonia/3_blinds/anomaly.lua
+++ content/team-eudaimonia/3_blinds/anomaly.lua
@@ -1,47 +1,54 @@
--- The Anomaly
+SMODS.Atlas({
+    key = "blinded",
+    path = "team-eudaimonia/blinds/anoblind.png",
+    px = 34,
+    py = 34,
+    frames = 21,
+    atlas_table = "ANIMATION_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "blinded",
+    path = "team-eudaimonia/blinds/anoblind.png",
+    px = 34,
+    py = 34,
+    frames = 21,
+    atlas_table = "ANIMATION_ATLAS"
+})
+
+SMODS.Sound {
+    key = "music_anomaly",
+    path = "music_anomaly.ogg",
+    select_music_track = function()
+        if G.GAME.blind and G.GAME.blind.config.blind.key == 'bl_worm_anomaly' then
+            return true
+        end
+    end
+}
+
+-- The Anomaly (Code by tby and m0xes)
 SMODS.Blind {
     key = "anomaly",
     dollars = 5,
-    mult = 1,
-    pos = { x = 0, y = 9 },
+    mult = 2,
+    pos = { x = 0, y = 0 },
     boss = { min = 1 },
-    boss_colour = HEX("8a59a5"),
-    config = {extra={tot_change=1}},
+    boss_colour = HEX("8a8a8a"),
+    atlas = 'blinded',
+    config = { extra = { tot_change = 1 } },
     percent_change = 0.2,
     loc_vars = function(self, info_queue, back)
-        return { vars = { self.percent_change*100} }
+        return { vars = { self.percent_change * 100 } }
     end,
-    disable = function(self)
-        G.GAME.blind.chips = G.GAME.blind.chips / G.GAME.blind.effect.extra.tot_change
-        G.GAME.blind.effect.extra.tot_change = 1
+    set_blind = function(self)
+        G.GAME.blind.mult = math.max(2, (2 + Wormhole.EUDA.anomaly_mult))
+        G.GAME.blind.chips = (G.GAME.blind.chips / 2) * G.GAME.blind.mult
         G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
     end,
+    disable = function(self)
+        G.GAME.blind.chips = (G.GAME.blind.chips / G.GAME.blind.mult) * 2
+    end,
     calculate = function(self, blind, context)
-        if  not blind.disabled and context.before then
-            G.GAME.blind.chips = G.GAME.blind.chips * (1 + self.percent_change)
-            G.GAME.blind.effect.extra.tot_change = G.GAME.blind.effect.extra.tot_change * (1 + self.percent_change)
-            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
-            blind.triggered = true
-            G.E_MANAGER:add_event(Event({
-                trigger = 'immediate',
-                func = (function()
-                    SMODS.juice_up_blind()
-                    G.E_MANAGER:add_event(Event({
-                        trigger = 'after',
-                        delay = 0.06 * G.SETTINGS.GAMESPEED,
-                        blockable = false,
-                        blocking = false,
-                        func = function()
-                            play_sound('tarot2', 0.76, 0.4)
-                            return true
-                        end
-                    }))
-                    play_sound('tarot2', 1, 0.4)
-                    return true
-                end)
-            }))
-            delay(0.4)
-        end
     end,
     in_pool = function(self) return false end,
 }
