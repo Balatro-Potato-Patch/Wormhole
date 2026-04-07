e24a8709f876f9813540654445fb9843bd5ba723
Foo45 <rowan.stjames.cooper@gmail.com>
2026-04-07 15:24:09 -0400

    updating changes

--- content/TheLastResort/2_constellation/8_hercules.lua
+++ content/TheLastResort/2_constellation/8_hercules.lua
@@ -1,16 +1,28 @@
 SMODS.Consumable{
 	set = "worm_tlr_constellation",
+	key = "tlr_const_hercules",
 	atlas = "tlr_const",
 	pos = {x = 0, y = 6},
 	config = {
 		blind = nil
 	},
 	loc_vars = function(self, info_queue, card)
-		return {vars = {card.ability.blind}}
+		if card.ability.blind then
+			info_queue[#info_queue+1] = {set = "Blind", key = card.ability.blind, vars = G.P_BLINDS[card.ability.blind].vars}
+		elseif card.ability.tier == 4 then
+			self:reset_blind(card)
+			info_queue[#info_queue+1] = {set = "Blind", key = card.ability.blind, vars = G.P_BLINDS[card.ability.blind].vars}
+		end
+		return {vars = {localize{set = "Blind", type = "name_text", key = card.ability.blind}}}
 	end,
 	reset_blind = function(self, card)
-		local blind = "bl_final_heart"
-		card.ability.blind = blind -- i forgot how to get blinds
+		local choices = {}
+		for key, blind in pairs(G.P_BLINDS) do
+			if blind.boss and not blind.boss.showdown then
+				choices[#choices+1] = key
+			end
+		end
+		card.ability.blind = pseudorandom_element(choices, "tlr_const_hercules")
 		card:juice_up()
 	end,
 	set_ability = function (self, card, initial, delay_sprites)
@@ -32,6 +44,29 @@ SMODS.Consumable{
 		return G.STATE == G.STATES.SHOP
 	end,
 	use = function (self, card, area, copier)
-		
+		if self.ability.tier == 4 and not self.ability.blind then
+			self:reset_blind(card)
+		end
+		G.E_MANAGER:add_event(Event({
+			trigger = 'immediate',
+			func = function ()
+				ease_round(1)
+				inc_career_stat('c_rounds', 1)
+				local blind = {"bl_small", "bl_big", "bl_big", card.ability.blind}
+				local ondeck = {"Small", "Big", "Big", "Boss"}
+				G.GAME.round_resets.blind_tag = nil
+				G.GAME.round_resets.blind = G.P_BLINDS[blind[card.ability.tier]]
+				G.GAME.round_resets.blind_states[ondeck[card.ability.tier]] = 'Current'
+				delay(0.2)
+				return true
+			end
+		}))
+		G.E_MANAGER:add_event(Event({
+			trigger = 'immediate',
+			func = function()
+				new_round()
+				return true
+			end
+		}))
 	end
 }
