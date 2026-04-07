f39e72db5f6ccdce8e23874d1c77d743dfdf5788
Amphivinut <youjerr7@gmail.com>
2026-04-07 13:26:55 -0700

    remove temp stargazing effect

--- content/TheLastResort/3_items/Vouchers/stargazing.lua
+++ content/TheLastResort/3_items/Vouchers/stargazing.lua
@@ -3,14 +3,5 @@ SMODS.Voucher {
 	set = 'Voucher',
 	pos = { x = 0, y = 0 },
 	cost = 10,
-    config = { extra = { rate = 8 } },
 
-    redeem = function(self, card)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                G.GAME.worm_tlr_constellation_rate = card.ability.extra.rate
-                return true
-            end
-        }))
-    end
 }
