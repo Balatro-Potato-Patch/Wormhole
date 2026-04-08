56798414592c928d8151e8cd9ee47c0d663ff5ef
LasagnaFelidae <87911854+LasagnaFelidae@users.noreply.github.com>
2026-04-08 07:43:06 +0200

    Update typ0_Things.lua

--- content/team-eudaimonia/2_jokers/typ0_Things.lua
+++ content/team-eudaimonia/2_jokers/typ0_Things.lua
@@ -238,7 +238,7 @@ SMODS.Consumable {
     cost = 3,
     atlas = "euda_bigearatlas",
     pos = { x = 0, y = 0 },
-    config = { hand_types = {'worm_pkr_wow','worm_pkr_wow_f'} },
+    config = {},
     ppu_coder = {'Typ0', 'LasagnaFelidae'},
     ppu_artist = {'LasagnaFelidae'},
     hidden = true,
@@ -279,7 +279,7 @@ SMODS.Consumable {
         }))
         update_hand_text({ sound = 'button', volume = 0.7, pitch = 0.9, delay = 0 }, { level = '+1' })
         delay(1.3)
-        SMODS.upgrade_poker_hands({hands = {"worm_pkr_euda_wow","worm_pkr_euda_wow_f"}, instant = true, from = "c_worm_euda_bigear"})
+        SMODS.upgrade_poker_hands({hands = {"worm_pkr_euda_wow","worm_pkr_euda_wow_f"}, instant = true, from = card})
         update_hand_text({ sound = 'button', volume = 0.7, pitch = 1.1, delay = 0 },
             { mult = 0, chips = 0, handname = '', level = '' })
     end,
