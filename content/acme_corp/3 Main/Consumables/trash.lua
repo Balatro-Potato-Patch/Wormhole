f783211e5c748befd59239371e0e574e8a3fbba3
Okay <183864149+OOkayOak@users.noreply.github.com>
2026-04-08 19:23:52 +0100

    fix: Trash starts at 0 as intended

--- content/acme_corp/3 Main/Consumables/trash.lua
+++ content/acme_corp/3 Main/Consumables/trash.lua
@@ -6,7 +6,7 @@ SMODS.Consumable{
     soul_pos = {x=5, y=1},
     ppu_artist = {'RadiationV2'},
     ppu_coder = {'Opal'},
-    config = {extra = {required = 30, discarded = 30}},
+    config = {extra = {required = 30, discarded = 0}},
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue+1] = {key = 'perishable', set = 'Other', vars = {5, 5}}
         if card.ability.extra.discarded < card.ability.extra.required then
