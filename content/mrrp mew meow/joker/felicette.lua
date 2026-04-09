f88bca5b20de060220baf65f4c81acb557de8969
CyanCelestial <cyancelestial@yahoo.com>
2026-04-08 17:13:37 -0700

    Made Felicette simpler
    Kept the original version commented out just in case

--- content/mrrp mew meow/joker/felicette.lua
+++ content/mrrp mew meow/joker/felicette.lua
@@ -1,3 +1,4 @@
+--[[ Scaling version ] ]
 SMODS.Joker {
     key = 'felicette',
     atlas = "mrrp",
@@ -58,13 +59,12 @@ SMODS.Joker {
     end,
 
     calculate = function(self, card, context)
-        if context.poker_hand_changed and context.old_level and context.new_level and not context.blueprint then
+        if context.poker_hand_changed and context.new_level and not context.blueprint then
             if context.new_level > 1 and not card.ability.extra.hands[context.scoring_name] then
                 card.ability.extra.hands[context.scoring_name] = true
                 SMODS.scale_card(card, {
                     ref_value = "mult",
                     scalar_value = "mult_mod",
-            --      message_colour = G.C.MULT,
                     no_message = true,
                 })
             elseif context.new_level <= 1 and card.ability.extra.hands[context.scoring_name] then
@@ -73,12 +73,6 @@ SMODS.Joker {
                     ref_value = "mult",
                     scalar_value = "mult_mod",
                     operation = '-',
-            --[[
-                    scaling_message = {
-                        message = localize("k_downgrade_ex"),
-                        colour = G.C.MULT
-                    }
-            ]]
                     no_message = true,
                 })
             end
@@ -86,6 +80,58 @@ SMODS.Joker {
         if context.joker_main and card.ability.extra.mult ~= 0 then
             return {
                 mult = card.ability.extra.mult,
+                card = context.blueprint and context.blueprint_card or card,
+                message_card = context.blueprint and context.blueprint_card or card
+            }
+        end
+    end
+}
+--[[ Simple version ]]
+SMODS.Joker {
+    key = 'felicette',
+    atlas = "mrrp",
+    pos = {
+        x = 2,
+        y = 1
+    },
+    rarity = 1,
+    cost = 5,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    attributes = {'cat'},
+    config = {
+        extra = {
+            mult_mod = 3,
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        local scalar = card.ability.extra.mult_mod
+        local upgrades = 0
+        for _, v in pairs(G.GAME.hands) do
+            if v.level and v.level > 1 then
+                upgrades = upgrades + 1
+            end
+        end
+        local total = scalar * upgrades
+        return {
+            vars = {
+                (scalar < 0 and "-" or "+") .. scalar,
+                (total < 0 and "-" or "+") .. total
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if context.joker_main and card.ability.extra.mult ~= 0 then
+            local upgrades = 0
+            for _, v in pairs(G.GAME.hands) do
+                if v.level and v.level > 1 then
+                    upgrades = upgrades + 1
+                end
+            end
+            return {
+                mult = card.ability.extra.mult_mod * upgrades,
                 card = context.blueprint and context.blueprint_card or card,
                 message_card = context.blueprint and context.blueprint_card or card
             }
