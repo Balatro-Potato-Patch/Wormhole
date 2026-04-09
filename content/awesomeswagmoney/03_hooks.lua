5a815dd481d84e77a7a57c4c283885fc8deb98a4
Eris (borb) <remcosveen@gmail.com>
2026-04-09 16:13:43 +0200

    initialize xurkitree value correctly

--- content/awesomeswagmoney/03_hooks.lua
+++ content/awesomeswagmoney/03_hooks.lua
@@ -1,11 +1,7 @@
 local reset_ref = Wormhole.reset_game_globals
-function Wormhole.reset_game_globals(run_start) --this doesnt work yet bcs someone was stupid
-    print"hello"
+function Wormhole.reset_game_globals(run_start)
     reset_ref(run_start)
     if run_start then
-        print"yes"
-        G.GAME.asm_xurkitree = 1
-    else
-        print"no"
+        G.GAME.asm_xurkitree = 7
     end
 end
