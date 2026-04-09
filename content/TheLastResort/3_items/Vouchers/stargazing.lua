496989e8a299f2657046156d7b0d00c61c743019
Amphivinut <youjerr7@gmail.com>
2026-04-09 13:06:30 -0700

    stargazing

--- content/TheLastResort/3_items/Vouchers/stargazing.lua
+++ content/TheLastResort/3_items/Vouchers/stargazing.lua
@@ -3,4 +3,13 @@ SMODS.Voucher {
 	set = 'Voucher',
 	pos = { x = 0, y = 0 },
 	cost = 10,
+
+	calculate = function(self, card, context)
+		if context.modify_shop_card and context.card.ability.set == 'worm_tlr_constellation' then
+			if pseudorandom('tlr_stargazing') > 0.67 then
+				context.card.ability.tier = context.card.ability.tier + 1
+				WORM_TLR.update_const_sprite(context.card.config.center, context.card)
+			end
+		end
+	end
 }
