a701ac03f912b91d8ed404eac42452add9969d9a
WilsontheWolf <git@shorty.systems>
2026-04-08 19:58:23 -0600

    credis

---
+++ localization/util-modders/en-us.lua
@@ -0,0 +1,34 @@
+print"hi mom"
+return {
+    descriptions = {
+	PotatoPatch = {
+	    t_util_modders = {
+		name = "https://shorty.systems/util-modders.webp",
+	    },
+	    d_wilson = {
+		name = "WilsontheWolf",
+		text = {
+		    "hi"
+		}
+	    },
+	    d_cg = {
+		name = "cg",
+		text = {
+		    "hi"
+		}
+	    },
+	    d_frost = {
+		name = "frost",
+		text = {
+		    "hi"
+		}
+	    },
+	    d_metherul = {
+		name = "metherul",
+		text = {
+		    "hi"
+		}
+	    },
+	}
+    }
+}
