12fe50214027f3a8c3ad8eb1b200ed56bb21c199
spencers145 <smartypantsspencer@gmail.com>
2026-04-09 05:09:42 -0400

    Change Drink Collection Rows to 5-5

--- content/absinthe/drinks.lua
+++ content/absinthe/drinks.lua
@@ -5,7 +5,7 @@ SMODS.ConsumableType {
     default = 'c_worm_abs_hubble_trouble',
     primary_colour = HEX('e3dbc8'),
     secondary_colour = HEX('f5c242'),
-    collection_rows = { 5, 4 },
+    collection_rows = { 5, 5 },
     shop_rate = 0.0
 }
 
