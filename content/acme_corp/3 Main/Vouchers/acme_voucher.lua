d8921df196feef9e730b05afe3c0e3e07b4a57a8
Juniper <basil.squared1@gmail.com>
2026-04-11 16:20:09 -0500

    remove rate increase from first voucher

--- content/acme_corp/3 Main/Vouchers/acme_voucher.lua
+++ content/acme_corp/3 Main/Vouchers/acme_voucher.lua
@@ -2,10 +2,8 @@ SMODS.Voucher {
     key = 'ACME_voucher',
     atlas = Atlas_AcmeVouchers.key,
     pos = { x = 0, y = 0 },
-    config = { extra = { rate_increase = 2 } },
-    loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.rate_increase } }
-    end,
+    config = { extra = {} },
+
 
 
 }
