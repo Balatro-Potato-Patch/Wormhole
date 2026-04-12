64bbbacadd857187020b2afedd95b75d45b6a06f
Professor Renderer Grizzleton <dimjcb@aol.com>
2026-04-11 21:54:35 -0400

    Update capitalization of text

--- localization/Hedonia/en-us.lua
+++ localization/Hedonia/en-us.lua
@@ -23,7 +23,7 @@ return {
             c_worm_hedonia_blackHoleBomb = {
                 name = 'Black Hole Bomb',
                 text = {
-                    'Makes a random card in hand blackout'
+                    'Makes a random card in hand blackout drunk'
                 }
             }
         },
@@ -31,7 +31,7 @@ return {
             p_worm_hedonia_menu_normal_1 = {
                 name = "Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -39,7 +39,7 @@ return {
             p_worm_hedonia_menu_normal_2 = {
                 name = "Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -47,7 +47,7 @@ return {
             p_worm_hedonia_menu_normal_3 = {
                 name = "Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -55,7 +55,7 @@ return {
             p_worm_hedonia_menu_normal_4 = {
                 name = "Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -63,7 +63,7 @@ return {
             p_worm_hedonia_menu_jumbo_1 = {
                 name = "Jumbo Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -71,7 +71,7 @@ return {
             p_worm_hedonia_menu_jumbo_2 = {
                 name = "Jumbo Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -79,7 +79,7 @@ return {
             p_worm_hedonia_menu_mega_1 = {
                 name = "Mega Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
@@ -87,7 +87,7 @@ return {
             p_worm_hedonia_menu_mega_2 = {
                 name = "Mega Menu Pack",
                 text = {
-                    "order {C:attention}#1#{} of up to",
+                    "Order {C:attention}#1#{} of up to",
                     "{C:attention}#2#{} Menu items to",
                     "be used immediately"
                 }
