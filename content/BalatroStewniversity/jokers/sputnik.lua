aecbfe74782edcfd1c630adf136fa170ef134365
PLagger <plagger1317@gmail.com>
2026-04-14 03:45:25 +0700

    atlas update + sputnik start

---
+++ content/BalatroStewniversity/jokers/sputnik.lua
@@ -0,0 +1,22 @@
+SMODS.Joker{ --Sputnik
+
+    ppu_team = {'Balatro Stewniversity'},
+    ppu_artist = {''},
+    ppu_coder = {'PLagger'},
+
+    key = 'worm_stew_sputnik',
+    config = {extra = {chips = 50}},
+    rarity = 3,
+    cost = 8,
+    atlas = 'stewjokers',
+    pos = {x=6, y=9},
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+
+    update_chips = function (self, card)
+        
+    end
+
+}
+local thunk = (G.GAME.interest_cap or 25) - math.floor((G.GAME.dollars or 0) / 5) 
