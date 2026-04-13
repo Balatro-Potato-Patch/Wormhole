f0f6a3482e4e63c5c28b7b042744ef023779eb96
PLagger <plagger1317@gmail.com>
2026-04-14 04:30:12 +0700

    sputnik needs loc

--- content/BalatroStewniversity/jokers/sputnik.lua
+++ content/BalatroStewniversity/jokers/sputnik.lua
@@ -5,18 +5,33 @@ SMODS.Joker{ --Sputnik
     ppu_coder = {'PLagger'},
 
     key = 'worm_stew_sputnik',
-    config = {extra = {chips = 50}},
+    config = {extra = {chips = 250, chips_mod = 50}},
     rarity = 3,
     cost = 8,
     atlas = 'stewjokers',
     pos = {x=6, y=9},
     blueprint_compat = true,
-    eternal_compat = true,
+    eternal_compat = true, 
     perishable_compat = true,
 
     update_chips = function (self, card)
-        
+        card.ability.extra.chips = card.ability.extra.chips_mod * math.max(0, (math.floor((G.GAME.interest_cap - G.GAME.dollars) / 5)))
+    end,
+
+    loc_vars = function (self, info_queue, card)
+        self:update_chips(card)
+        return{
+            vars = {card.ability.extra.chips, card.ability.extra.chips_mod}
+        }
+    end,
+
+    calculate = function (self, card, context)
+        if context.joker_main then
+            self:update_chips(card)
+            return{
+                chips = card.ability.extra.chips
+            }
+        end
     end
 
 }
--- local thunk = (G.GAME.interest_cap or 25) - math.floor((G.GAME.dollars or 0) / 5) 
