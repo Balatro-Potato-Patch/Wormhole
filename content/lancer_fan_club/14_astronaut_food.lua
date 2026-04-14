3cdbe208aea8f6e869f76a6ad1338161fb89a84d
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2026-04-14 14:01:01 -0400

    fix astronaut food

--- content/lancer_fan_club/14_astronaut_food.lua
+++ content/lancer_fan_club/14_astronaut_food.lua
@@ -1,3 +1,7 @@
+Wormhole.LancerFanClub.levels_all_hands = {
+	c_black_hole = true,
+}
+
 SMODS.Joker({
 	key = "lfc_astronaut_food",
 	pos = { x = 1, y = 2 },
@@ -6,7 +10,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	demicoloncompat = false,
 	atlas = "lfc_jokers",
-	ppu_coder = { "ellestuff." },
+	ppu_coder = { "ellestuff.", "InvalidOS" },
 	ppu_artist = { "J8-Bit" },
 	ppu_team = { "Lancer Fan Club" },
 	config = {
@@ -31,7 +35,7 @@ SMODS.Joker({
 		} }
 	end,
 	calculate = function(self, card, context)
-		if context.poker_hand_changed and context.card.config.center_key ~= "j_worm_lfc_astronaut_food" then
+		if context.poker_hand_changed and context.card.config.center.key ~= "j_worm_lfc_astronaut_food" then
 			local count = math.min(context.new_level-context.old_level,card.ability.extra.remaining)
 			SMODS.upgrade_poker_hands {
 				hands = context.scoring_name,
@@ -45,9 +49,11 @@ SMODS.Joker({
 						return base+card.ability.extra.mult*level_up*count
 					end
 				end,
-				from = card
+				from = card,
+				instant = context.instant
 			}
 			
+			if context.blueprint or Wormhole.LancerFanClub.levels_all_hands[context.card.config.center.key] then return end
 			if card.ability.extra.remaining <= count then
 				SMODS.destroy_cards(card, nil, nil, false)
 
@@ -60,3 +66,21 @@ SMODS.Joker({
 		end
 	end
 })
+
+-- make black hole count as only leveling up one hand
+local suph = SMODS.upgrade_poker_hands
+function SMODS.upgrade_poker_hands(args, ...)
+	suph(args, ...)
+	if args.from and ((not args.hands) or #args.hands > 1) and Wormhole.LancerFanClub.levels_all_hands[args.from.config.center.key] then
+		print("bhole")
+		for _, card in ipairs(SMODS.find_card("j_worm_lfc_astronaut_food")) do
+			local count = math.min(args.level_up or 0, card.ability.extra.remaining)
+			if card.ability.extra.remaining <= count then
+				SMODS.destroy_cards(card, nil, nil, false)
+				SMODS.calculate_effect({message = localize('k_eaten_ex')}, card)
+			else
+				card.ability.extra.remaining = card.ability.extra.remaining-count
+			end
+		end
+	end
+end
