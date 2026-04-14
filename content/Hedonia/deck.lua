8f91086ceac7347425ca27db352415946cdc2ad5
Axyraandas <digatadefender495@gmail.com>
2026-04-14 18:49:54 -0400

    adjusted deck apply code

--- content/Hedonia/deck.lua
+++ content/Hedonia/deck.lua
@@ -10,11 +10,20 @@ SMODS.Back {
     atlas = 'deck',
     pos = {x=0, y=0},
     apply = function(self, back)
-
-        SMODS.add_card({ set = "Bartender", area = G.Jokers}) --https://github.com/SpectralPack/Cryptid/blob/58a5625f9bba973cdb9ae5e3abde4112276fe1a4/items/misc_joker.lua#L10259
-
-        --[[https://github.com/RAYTRAC3R/AdventureTro/blob/e4e80e61b25391e43d6255a2fbbdc4c1e5d8d06e/items/Decks.lua#L26
-        local card_to_create = pseudorandom_element("Bartender", "hedonia_bar")
-        local card = SMODS.create_card({set="Joker", area=G.jokers, key=card_to_create})]]
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                if G.jokers then
+                    local candidates = {}
+                    for i,v in pairs(G.P_CENTER_POOLS.Joker) do
+                        if v.pools and v.pools.Bartender then
+                            candidates[#candidates+1] = v.key
+                        end
+                    end
+                    
+                    SMODS.add_card({area = G.jokers, key = pseudorandom_element(candidates)})
+                end
+                return true
+            end
+        }))
     end
 }
