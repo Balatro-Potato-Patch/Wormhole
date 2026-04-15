c09b5bcf65177e76aa803b72c87de59d664452c7
Abel-sketch <155697463+Abel-sketch@users.noreply.github.com>
2026-04-14 20:16:02 -0500

    Text box for credits thing

--- localization/JuryRigged/en-us.lua
+++ localization/JuryRigged/en-us.lua
@@ -274,7 +274,7 @@ return {
       PotatoPatchDev_Maelmc = { name = "Maelmc", text = { "Insert something here" } },
       PotatoPatchDev_Inky = { name = "Inky", text = { "Got something to say?" } },
       PotatoPatchDev_DoggFly = { name = "DoggFly", text = { "Just here to take up space" } },
-      PotatoPatchDev_AbelSketch = { name = "AbelSketch", text = { "Did you know that strings are immutable in lua" } },
+PotatoPatchDev_AbelSketch = { name = "AbelSketch", text = { {"{C:attention}Fun Fact:{} Theres a reason to not share water bottles!" },{"I got Salmonela from one... 3:"} }},
       PotatoPatchDev_Blanthos = { name = "Blanthos", text = { " First we Gneep, then we Gnarp. Lets Gneep Gnarp." } },
       PotatoPatchDev_NinjaBanana = { name = "NinjaBanana", text = { "Something about a bright one" } },
     }
