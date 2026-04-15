15c6234f43e5966158a8d879f4e2179f1476249c
Professor Renderer Grizzleton <dimjcb@aol.com>
2026-04-15 01:03:40 -0400

    Fixed booster pack music

--- content/Hedonia/sounds.lua
+++ content/Hedonia/sounds.lua
@@ -1,8 +1,8 @@
 SMODS.Sound({
     key = "hedonia_tonightsmenu_music",
-    path = "Hedonia/music_tonightsmenu.ogg",
-    pitch = 1,
-    volume = 1,
+    path = "music_tonightsmenu.ogg",
+    pitch = 0.7,
+    volume = 0.6,
     select_music_track = function()
         return G.booster_pack and not G.booster_pack.REMOVED and SMODS.OPENED_BOOSTER and SMODS.OPENED_BOOSTER.config.center.kind == 'hedonia_menu' and 100 or nil
     end,
