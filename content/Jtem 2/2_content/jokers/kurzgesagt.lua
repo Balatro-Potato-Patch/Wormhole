fb271c5c8edb3b2426730a345fc0e9a9cee9af56
makatymba2001 <makatymba2001@yandex.ru>
2026-04-15 18:13:06 +0300

    info_queue for jonkler

--- content/Jtem 2/2_content/jokers/kurzgesagt.lua
+++ content/Jtem 2/2_content/jokers/kurzgesagt.lua
@@ -5,7 +5,7 @@ SMODS.Atlas({
 	py = 95,
 })
 
-SMODS.Joker {
+SMODS.Joker({
 	atlas = "jtem2_kurzgesagt",
 	key = "jtem2_kurzgesagt",
 	ppu_team = { "jtem2" },
@@ -14,32 +14,39 @@ SMODS.Joker {
 	cost = 10,
 	rarity = 3,
 	blueprint_compat = false,
-	config = {
-	},
-	loc_vars = function (self, info_queue, card)
-		
+	config = {},
+	loc_vars = function(self, info_queue, card)
+		info_queue[#info_queue + 1] = G.P_CENTERS.m_steel
+		info_queue[#info_queue + 1] = G.P_CENTERS.m_gold
+		info_queue[#info_queue + 1] = G.P_CENTERS.m_stone
+		info_queue[#info_queue + 1] = G.P_CENTERS.m_worm_jtem2_strange_card
+		info_queue[#info_queue + 1] = G.P_CENTERS.m_worm_jtem2_gravacard
+		info_queue[#info_queue + 1] = G.P_CENTERS.m_worm_jtem2_neutron_card
 	end,
-    calculate = function(self, card, context)
-        if context.before and not context.blueprint then
-            for _, scored_card in ipairs(context.scoring_hand) do
-                if next(SMODS.get_enhancements(scored_card)) and not scored_card.debuff and not scored_card.vampired then
+	calculate = function(self, card, context)
+		if context.before and not context.blueprint then
+			for _, scored_card in ipairs(context.scoring_hand) do
+				if
+					next(SMODS.get_enhancements(scored_card))
+					and not scored_card.debuff
+					and not scored_card.vampired
+				then
 					local ench = SMODS.get_enhancements(scored_card)
 					if ench["m_steel"] then
-                    	scored_card:set_ability('m_worm_jtem2_strange_card', nil, true)
+						scored_card:set_ability("m_worm_jtem2_strange_card", nil, true)
 					elseif ench["m_gold"] then
-                    	scored_card:set_ability('m_worm_jtem2_gravacard', nil, true)
+						scored_card:set_ability("m_worm_jtem2_gravacard", nil, true)
 					elseif ench["m_stone"] then
-                    	scored_card:set_ability('m_worm_jtem2_neutron_card', nil, true)
+						scored_card:set_ability("m_worm_jtem2_neutron_card", nil, true)
 					end
-                    G.E_MANAGER:add_event(Event({
-                        func = function()
-                            scored_card:juice_up()
-                            return true
-                        end
-                    }))
-                end
-            end
-        end
-    end,
-
-}
+					G.E_MANAGER:add_event(Event({
+						func = function()
+							scored_card:juice_up()
+							return true
+						end,
+					}))
+				end
+			end
+		end
+	end,
+})
