47954a77dbc6dcc64a3bb1542d475d49dc25fe5e
WombatCountry <53626867+wombatCountry@users.noreply.github.com>
2026-04-14 20:29:39 -0400

    fixed crash

--- content/Hedonia/team.lua
+++ content/Hedonia/team.lua
@@ -1,3 +1,10 @@
+SMODS.Atlas {
+    key = 'credits',
+    px = 71,
+    py = 95,
+    path = "Hedonia/team.png"
+}
+
 PotatoPatchUtils.Team {
   name = 'Hedonia',
   colour = HEX("deb009"),
@@ -7,36 +14,48 @@ PotatoPatchUtils.Team {
 
 PotatoPatchUtils.Developer {
   name = 'alxndr',
+  atlas = 'worm_credits',
+  pos = {x = 0, y = 0},
   loc = true,
   team = 'Hedonia'
 }
 
 PotatoPatchUtils.Developer {
   name = 'axyraandas',
+  atlas = 'worm_credits',
+  pos = {x=1, y=0},
   loc = true,
   team = 'Hedonia'
 }
 
 PotatoPatchUtils.Developer {
   name = 'hellboydante',
+  atlas = 'worm_credits',
+  pos = {x=2, y=0},
   loc = true,
   team = 'Hedonia'
 }
 
 PotatoPatchUtils.Developer {
   name = 'professorrenderer',
+  atlas = 'worm_credits',
+  pos = {x=0, y=1},
   loc = true,
   team = 'Hedonia'
 }
 
 PotatoPatchUtils.Developer {
   name = 'qunumeru',
+  atlas = 'worm_credits',
+  pos = {x=1, y=1},
   loc = true,
   team = 'Hedonia'
 }
 
 PotatoPatchUtils.Developer {
   name = 'wombatcountry',
+  atlas = 'worm_credits',
+  pos = {x=2, y=1},
   loc = true,
   team = 'Hedonia'
 }
