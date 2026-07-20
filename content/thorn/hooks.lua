123f2dc8c6c336cf3663614ad59dfa93868d433f
the-Astra <djs8743@rit.edu>
2026-07-20 16:50:09 -0400

    Fix #140

--- content/thorn/hooks.lua
+++ content/thorn/hooks.lua
@@ -2,16 +2,19 @@ local upgrade_hook = SMODS.upgrade_poker_hands
 function SMODS.upgrade_poker_hands(args)
     if next(SMODS.find_card("j_worm_thorn_binary")) and args.from and args.from.config and args.from.config.center and args.from.config.center.set == "Planet" then
         local level_down = false
+        local upgraded_hands = type(args.hands) == 'string' and {args.hands} or args.hands
         for k, v in pairs(SMODS.find_card("j_worm_thorn_binary")) do
             if v.area.config.type == "joker" and not level_down then
-                if SMODS.pseudorandom_probability(v, 'thorn_binary', 1, v.ability.extra.odds) then
-                    args.level_up = args.level_up + 1
-                elseif G.GAME.hands[args.from.ability.hand_type].level > 1 then
-                    args.level_up = -1
-                    level_down = true
-                else
-                    args.level_up = 0
-                    level_down = true
+                for _, hand in pairs(upgraded_hands) do
+                    if SMODS.pseudorandom_probability(v, 'thorn_binary', 1, v.ability.extra.odds) then
+                        args.level_up = args.level_up + 1
+                    elseif G.GAME.hands[hand].level > 1 then
+                        args.level_up = -1
+                        level_down = true
+                    else
+                        args.level_up = 0
+                        level_down = true
+                    end
                 end
             end
         end
